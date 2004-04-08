6bd2c47f7c24929ca28f41284fa8e1af81d20998
Michal Čihař <michal@cihar.com>
2004-04-08 22:37:21 +0200

    Imported gammu 0.94.04

--- cfg/config.h
+++ cfg/config.h
@@ -6,8 +6,8 @@
 /* -------------------------- Gammu specific ---------------------------- */
 
 /* Version of package */
-#define VERSION "0.94.2"
-#define VERSION_WIN "0,94,2,0"
+#define VERSION "0.94.4"
+#define VERSION_WIN "0,94,4,0"
 
 #ifndef _MSC_VER
 /* Define if want DEBUG info */
