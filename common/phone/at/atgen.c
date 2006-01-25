bc8303adb7a5a3515dbd46f7c583514d1fffe8db
Michal Čihař <michal@cihar.com>
2006-01-25 21:12:02 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-107] fix sms sending - report error using callback functions

--- common/phone/at/atgen.c
+++ common/phone/at/atgen.c
@@ -2079,11 +2079,19 @@ GSM_Error ATGEN_ReplySendSMS(GSM_Protocol_Message msg, GSM_StateMachine *s)
 		return ERR_NONE;
 	case AT_Reply_CMSError:
  		smprintf(s, "Error %i\n",Priv->ErrorCode);
- 		if (s->User.SendSMSStatus!=NULL) s->User.SendSMSStatus(s->CurrentConfig->Device,Priv->ErrorCode,-1);
+ 		if (s->User.SendSMSStatus != NULL) {
+			s->User.SendSMSStatus(s->CurrentConfig->Device, Priv->ErrorCode, -1);
+		}
  		return ATGEN_HandleCMSError(s);
 	case AT_Reply_Error:
+ 		if (s->User.SendSMSStatus != NULL) {
+			s->User.SendSMSStatus(s->CurrentConfig->Device, -1, -1);
+		}
 		return ERR_UNKNOWN;
 	default:
+ 		if (s->User.SendSMSStatus != NULL) {
+			s->User.SendSMSStatus(s->CurrentConfig->Device, -1, -1);
+		}
 		return ERR_UNKNOWNRESPONSE;
 	}
 }
