a4487c0f79ec842b846ff4a3d21f2010c10f67ac
Michal Čihař <michal@cihar.com>
2006-11-10 23:13:14 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-164] add forgotten header - this belongs to previous commit

---
+++ common/phone/obex/obexgen-functions.h
@@ -0,0 +1,21 @@
+/* (c) 2002-2006 by Marcin Wiacek and Michal Cihar */
+
+#ifndef obexgen_functions_h
+#define obexgen_functions_h
+
+#include "../../gsmcomon.h"
+#include "../../gsmstate.h"
+#include "../../service/sms/gsmsms.h"
+
+extern GSM_Reply_Function	OBEXGENReplyFunctions[];
+extern GSM_Error OBEXGEN_GetFilePart	(GSM_StateMachine *s, GSM_File *File, int *Handle, int *Size);
+extern GSM_Error OBEXGEN_AddFilePart	(GSM_StateMachine *s, GSM_File *File, int *Pos, int *Handle);
+extern GSM_Error OBEXGEN_Disconnect	(GSM_StateMachine *s);
+extern GSM_Error OBEXGEN_Connect	(GSM_StateMachine *s, OBEX_Service service);
+
+
+#endif
+
+/* How should editor hadle tabs in this file? Add editor commands here.
+ * vim: noexpandtab sw=8 ts=8 sts=8:
+ */
