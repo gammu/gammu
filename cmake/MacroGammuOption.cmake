a5a95e7235d1f62cd46b3222272a8f3b93519007
Michal Čihař <michal@cihar.com>
2007-01-27 15:27:00 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-315] improve cmake - detect irda - really allow disabling of whole subsets (eg. Nokia phones, Bluetooth, ...)

--- cmake/MacroGammuOption.cmake
+++ cmake/MacroGammuOption.cmake
@@ -1,9 +1,17 @@
 
-MACRO (MACRO_GAMMU_OPTION _name _description _default)
-    OPTION(WITH_${_name} "Whether to enable ${_description}" ${_default})
-    if (WITH_${_name})
-        message("${_description} enabled")
-        set(GSM_ENABLE_${_name} TRUE)
-    endif (WITH_${_name})
+set (ON ON)
+
+MACRO (MACRO_GAMMU_OPTION _name _description _default _cond1 _cond2)
+    if (${_cond1} AND ${_cond2})
+        OPTION(WITH_${_name} "Whether to enable ${_description}" ${_default})
+        if (WITH_${_name})
+            message("${_description} enabled")
+            set(GSM_ENABLE_${_name} TRUE)
+        else (WITH_${_name})
+            set(GSM_ENABLE_${_name})
+        endif (WITH_${_name})
+    else (${_cond1} AND ${_cond2})
+        set(GSM_ENABLE_${_name})
+    endif (${_cond1} AND ${_cond2})
 ENDMACRO (MACRO_GAMMU_OPTION)
 
