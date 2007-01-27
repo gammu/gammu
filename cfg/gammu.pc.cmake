422a7f85dc4c2202898cc24a51f986c03b3f24a0
Michal Čihař <michal@cihar.com>
2007-01-27 14:00:25 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-310] cmake pkgconfig support

---
+++ cfg/gammu.pc.cmake
@@ -0,0 +1,10 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+libdir=${exec_prefix}/@INSTALL_LIB_DIR@
+includedir=${prefix}/@INSTALL_INC_DIR@
+
+Name: gammu
+Description: Gammu library
+Version: @VERSION@
+Libs: -L${libdir} -lGammu -lm
+Cflags: -I${includedir}/gammu
