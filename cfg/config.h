af55a7e25973d8be12a1928093d9e8b94b697ba4
Michal Čihař <michal@cihar.com>
2007-01-27 16:35:08 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-319] increase version to 1.09.18

--- cfg/config.h
+++ cfg/config.h
@@ -6,8 +6,8 @@
 /* -------------------------- Gammu specific ---------------------------- */
 
 /* Version of package */
-#define VERSION "1.09.17"
-#define VERSION_WIN "1,09,17,0"
+#define VERSION "1.09.18"
+#define VERSION_WIN "1,09,18,0"
 
 #ifndef _MSC_VER
 /* Define if want DEBUG info */
