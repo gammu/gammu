ec61576a38899f1c63cafb79efa89bdc98fe720f
Michal Čihař <michal@cihar.com>
2007-02-04 13:22:46 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-330] enable bluetooth support when using MinGW

--- common/device/bluetoth/blue_w32.h
+++ common/device/bluetoth/blue_w32.h
@@ -1,11 +1,11 @@
+#ifndef __blue_w32_h
+#define __blue_w32_h
 
 GSM_Error bluetooth_connect(GSM_StateMachine *s, int port, char *device);
 GSM_Error bluetooth_findchannel(GSM_StateMachine *s);
 
+#include <winsock2.h>
 /* MS Platform SDK */
-
-#ifndef __blue_w32_h
-#define __blue_w32_h
 
 #include <pshpack1.h>		// Without it compiled code hangs up BT stack
 
@@ -35,56 +35,6 @@ typedef struct _SOCKADDR_BTH {
 
 #ifdef BLUETOOTH_RF_SEARCHING
 
-typedef struct _SOCKET_ADDRESS {
-    LPSOCKADDR 		lpSockaddr ;
-    INT 		iSockaddrLength ;
-} SOCKET_ADDRESS, *PSOCKET_ADDRESS, FAR * LPSOCKET_ADDRESS ;
-
-typedef struct _CSADDR_INFO {
-    SOCKET_ADDRESS 	LocalAddr ;
-    SOCKET_ADDRESS 	RemoteAddr ;
-    INT 		iSocketType ;
-    INT 		iProtocol ;
-} CSADDR_INFO, *PCSADDR_INFO, FAR * LPCSADDR_INFO ;
-
-typedef struct _AFPROTOCOLS {
-    INT iAddressFamily;
-    INT iProtocol;
-} AFPROTOCOLS, *PAFPROTOCOLS, *LPAFPROTOCOLS;
-
-typedef enum _WSAEcomparator
-{
-    COMP_EQUAL = 0,
-    COMP_NOTLESS
-} WSAECOMPARATOR, *PWSAECOMPARATOR, *LPWSAECOMPARATOR;
-
-typedef struct _WSAVersion
-{
-    DWORD           dwVersion;
-    WSAECOMPARATOR  ecHow;
-}WSAVERSION, *PWSAVERSION, *LPWSAVERSION;
-
-typedef struct _WSAQuerySetA
-{
-    DWORD           dwSize;
-    LPSTR           lpszServiceInstanceName;
-    LPGUID          lpServiceClassId;
-    LPWSAVERSION    lpVersion;
-    LPSTR           lpszComment;
-    DWORD           dwNameSpace;
-    LPGUID          lpNSProviderId;
-    LPSTR           lpszContext;
-    DWORD           dwNumberOfProtocols;
-    LPAFPROTOCOLS   lpafpProtocols;
-    LPSTR           lpszQueryString;
-    DWORD           dwNumberOfCsAddrs;
-    LPCSADDR_INFO   lpcsaBuffer;
-    DWORD           dwOutputFlags;
-    LPBLOB          lpBlob;
-} WSAQUERYSET, WSAQUERYSETA, *PWSAQUERYSETA, *LPWSAQUERYSETA;
-
-DEFINE_GUID(L2CAP_PROTOCOL_UUID,  0x00000100, 0x0000, 0x1000, 0x80, 0x00, 0x00, 0x80, 0x5F, 0x9B, 0x34, 0xFB);
-
 #ifndef NS_BTH
 #  define NS_BTH  16
 #endif
@@ -97,8 +47,6 @@ DEFINE_GUID(L2CAP_PROTOCOL_UUID,  0x00000100, 0x0000, 0x1000, 0x80, 0x00, 0x00, 
 #define LUP_RETURN_BLOB         0x0200
 #define LUP_FLUSHCACHE       	0x1000
 #define LUP_RES_SERVICE         0x8000
-
-#define WSAAPI                  FAR PASCAL
 
 #ifndef WINSOCK_API_LINKAGE
 #ifdef DECLSPEC_IMPORT
@@ -130,41 +78,6 @@ WSALookupServiceNextA(
 WINSOCK_API_LINKAGE INT WSAAPI WSALookupServiceEnd(IN HANDLE hLookup);
 
 #define MAX_PROTOCOL_CHAIN 7
-
-typedef struct _WSAPROTOCOLCHAIN {
-    int ChainLen;                                 /* the length of the chain,     */
-                                                  /* length = 0 means layered protocol, */
-                                                  /* length = 1 means base protocol, */
-                                                  /* length > 1 means protocol chain */
-    DWORD ChainEntries[MAX_PROTOCOL_CHAIN];       /* a list of dwCatalogEntryIds */
-} WSAPROTOCOLCHAIN, FAR * LPWSAPROTOCOLCHAIN;
-
-#define WSAPROTOCOL_LEN  255
-
-typedef struct _WSAPROTOCOL_INFOA {
-    DWORD 		dwServiceFlags1;
-    DWORD 		dwServiceFlags2;
-    DWORD 		dwServiceFlags3;
-    DWORD 		dwServiceFlags4;
-    DWORD 		dwProviderFlags;
-    GUID 		ProviderId;
-    DWORD 		dwCatalogEntryId;
-    WSAPROTOCOLCHAIN 	ProtocolChain;
-    int 		iVersion;
-    int 		iAddressFamily;
-    int 		iMaxSockAddr;
-    int 		iMinSockAddr;
-    int 		iSocketType;
-    int 		iProtocol;
-    int 		iProtocolMaxOffset;
-    int 		iNetworkByteOrder;
-    int 		iSecurityScheme;
-    DWORD 		dwMessageSize;
-    DWORD 		dwProviderReserved;
-    CHAR   		szProtocol[WSAPROTOCOL_LEN+1];
-} WSAPROTOCOL_INFOA, FAR * LPWSAPROTOCOL_INFOA;
-
-typedef WSAPROTOCOL_INFOA WSAPROTOCOL_INFO;
 
 #define SO_PROTOCOL_INFOA 0x2004      /* WSAPROTOCOL_INFOA structure */
 #define SO_PROTOCOL_INFO  SO_PROTOCOL_INFOA
