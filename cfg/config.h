cbf9d10e5260d8708f21cefb079f8573d7d88d98
Michal Čihař <michal@cihar.com>
2007-02-06 16:45:43 +0000

    [gammu @ michal@cihar.com--2005-main/gammu--fixes--1--patch-351] prepare for 1.09.20

--- cfg/config.h
+++ cfg/config.h
@@ -6,8 +6,8 @@
 /* -------------------------- Gammu specific ---------------------------- */
 
 /* Version of package */
-#define VERSION "1.09.19"
-#define VERSION_WIN "1,09,19,0"
+#define VERSION "1.09.20"
+#define VERSION_WIN "1,09,20,0"
 
 #ifndef _MSC_VER
 /* Define if want DEBUG info */
--- gammu.spec
+++ gammu.spec
@@ -1,4 +1,4 @@
-%define ver         1.09.19
+%define ver         1.09.20
 %define name        gammu
 %define bluetooth   1
 %define relnum      1
