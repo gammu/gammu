a49f9fa0d3749fcc4a1cfa74c61151204eba9895
Michal Čihař <michal@cihar.com>
2007-04-16 13:46:24 +0000

    remove stalled files and directories after conversion

--- other/php3/index.php
+++
@@ -1,6 +0,0 @@
-<?
-## to make everything work, you must schedule this file to run periodically
-## I crontabbed this sript to run every 30 secs
-require('config.php');
-$teste = $gammu->processnew();
-?>
