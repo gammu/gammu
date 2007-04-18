f27124e0db0c51086bc7ca2a45556d45b59e3d00
Michal Čihař <michal@cihar.com>
2007-04-18 11:26:24 +0000

    Tag release 0.8

---
+++ RELEASE_0_8/misc.h
@@ -0,0 +1,22 @@
+/*
+ * python-gammu - Phone communication libary
+ * Copyright (c) 2003 - 2005 Michal Čihař
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * Copies at most n chars from src to dest, dest will be \0 terminated.
+ */
+char *mystrncpy(char *dest, const char *src, size_t n);
