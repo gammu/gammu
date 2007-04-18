e715ffec24019512ddc7e169aec8c486d6e051e7
Michal Čihař <michal@cihar.com>
2007-04-18 11:34:26 +0000

    Tag release 0.19

---
+++ RELEASE_0_19/examples/sms-encode-decode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Simple test for testing SMS decoding/encoding
+
+import gammu
+
+# text of sms
+txt = '.........1.........2.........3.........4.........5.........6.........7.........8.........9........0.........1.........2.........3.........4.........5.........6.........7.........8.........9........0.........1.........2.........3.........4.........5.........6.........7.........8.........9........0'
+
+# SMS info about message
+smsinfo = {'Entries':[{'ID': 'ConcatenatedTextLong', 'Buffer': txt}]}
+
+# encode SMS
+sms = gammu.EncodeSMS(smsinfo)
+
+# decode back SMS
+decodedsms = gammu.DecodeSMS(sms)
+
+# show results
+print "Text:"
+print decodedsms['Entries'][0]['Buffer']
+print "Comparsion:"
+print (decodedsms['Entries'][0]['Buffer'] == txt)
