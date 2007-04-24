238e6a9dd88aaf0a47034602ae93b96f180259f3
Michal Čihař <michal@cihar.com>
2007-04-24 14:15:12 +0000

    add minimalistic title page

---
+++ cfg/mainpage.desc.cmake
@@ -0,0 +1,13 @@
+/**
+ * \mainpage Gammu
+ * \version @VERSION@
+ * \author Michal Čihař
+ *
+ * \em Gammu is library for communicating with (mostly) GSM phones.
+ * You can interface to it by using members of \ref GSM_StateMachine
+ * structure.
+ */
+/* How should editor hadle tabs in this file? Add editor commands here.
+ * vim: ft=c smartindent:
+ * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
+ */
