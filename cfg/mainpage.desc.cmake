15c06d7f40f60a456f19357c1cb801589a3cedad
Michal Čihař <michal@cihar.com>
2007-04-24 14:34:03 +0000

    improve a bit title page

--- cfg/mainpage.desc.cmake
+++ cfg/mainpage.desc.cmake
@@ -3,10 +3,11 @@
  * \version @VERSION@
  * \author Michal Čihař
  *
- * \em Gammu is library for communicating with (mostly) GSM phones.
+ * \b Gammu is library for communicating with (mostly) GSM phones.
  * You can interface to it by using members of \ref GSM_StateMachine
  * structure.
  */
+
 /* How should editor hadle tabs in this file? Add editor commands here.
  * vim: ft=c smartindent:
  * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
