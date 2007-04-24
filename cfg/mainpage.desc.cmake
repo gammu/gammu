9df0768fd4276e49d5caed45431cb1f9b093c10a
Michal Čihař <michal@cihar.com>
2007-04-24 14:38:46 +0000

    add disclaimer about incomplete documentation ;-)

--- cfg/mainpage.desc.cmake
+++ cfg/mainpage.desc.cmake
@@ -6,6 +6,11 @@
  * \b Gammu is library for communicating with (mostly) GSM phones.
  * You can interface to it by using members of \ref GSM_StateMachine
  * structure.
+ *
+ * This documentation is still not complete, however we try to complete
+ * documentation to every code part we touch. If you don't userstand
+ * something, please don't hesitate to ask on mailing list
+ * gammu-users@lists.sourceforge.net.
  */
 
 /* How should editor hadle tabs in this file? Add editor commands here.
