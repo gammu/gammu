5b0556716285f52de9d53a9cccf5620a768d0cff
Michal Čihař <michal@cihar.com>
2007-04-30 14:32:35 +0000

    Increase version to 1.10.6.

--- cfg/config.h
+++ cfg/config.h
@@ -6,8 +6,8 @@
 /* -------------------------- Gammu specific ---------------------------- */
 
 /* Version of package */
-#define VERSION "1.10.5"
-#define VERSION_WIN "1,10,5,0"
+#define VERSION "1.10.6"
+#define VERSION_WIN "1,10,6,0"
 
 #ifndef _MSC_VER
 /* Define if want DEBUG info */
