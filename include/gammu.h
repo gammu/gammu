26efaabde59b6ab7bf224d253bde460385bb90e1
Michal Čihař <michal@cihar.com>
2007-05-16 14:23:56 +0000

    More API conversion.

--- include/gammu.h
+++ include/gammu.h
@@ -31,7 +31,7 @@ typedef struct _GSM_StateMachine 	GSM_StateMachine;
  * \param ReplyNum Number of replies to await (usually 3).
  * \return Error code
  */
-GSM_Error GSM_InitConnection		(GSM_StateMachine *s, int ReplyNum);
+GSM_Error GSM_InitConnection(GSM_StateMachine *s, int ReplyNum);
 
 /**
  * Terminates connection.
@@ -41,8 +41,20 @@ GSM_Error GSM_InitConnection		(GSM_StateMachine *s, int ReplyNum);
  * \param s State machine data
  * \return Error code
  */
-GSM_Error GSM_TerminateConnection	(GSM_StateMachine *s);
+GSM_Error GSM_TerminateConnection(GSM_StateMachine *s);
 
+/**
+ * Attempts to read data from phone. This can be used for getting 
+ * status of incoming events, which would not be found out without 
+ * polling device.
+ *
+ * \ingroup StateMachine
+ *
+ * \param s State machine data
+ * \param wait Whether to wait for some event
+ * \return Number of read bytes
+ */
+int GSM_ReadDevice(GSM_StateMachine *s, bool wait);
 #endif
 /* Editor configuration
  * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
