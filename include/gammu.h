ebfd5e7d05729fcd537a278cbaca5de395507422
Michal Čihař <michal@cihar.com>
2007-05-16 14:16:19 +0000

    More headers conversion.

--- include/gammu.h
+++ include/gammu.h
@@ -4,8 +4,8 @@
  * 
  * Top level include for applications.
  */
-#ifndef API_GAMMU_H
-#define API_GAMMU_H
+#ifndef __gammu_gammu_h
+#define __gammu_gammu_h
 
 struct _GSM_StateMachine;
 
@@ -13,13 +13,35 @@ struct _GSM_StateMachine;
  * \defgroup StateMachine State machine
  * Generic state machine layer.
  */
+
 /**
  * Private structure holding information about phone connection.
  * \ingroup StateMachine
  */
 typedef struct _GSM_StateMachine 	GSM_StateMachine;
 
+#include <error.h>
 
+/**
+ * Initiates connection.
+ *
+ * \ingroup StateMachine
+ *
+ * \param s State machine data
+ * \param ReplyNum Number of replies to await (usually 3).
+ * \return Error code
+ */
+GSM_Error GSM_InitConnection		(GSM_StateMachine *s, int ReplyNum);
+
+/**
+ * Terminates connection.
+ *
+ * \ingroup StateMachine
+ *
+ * \param s State machine data
+ * \return Error code
+ */
+GSM_Error GSM_TerminateConnection	(GSM_StateMachine *s);
 
 #endif
 /* Editor configuration
