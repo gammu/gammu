4449940c7fb863662c6b5843536147a922adf60e
Michal Čihař <michal@cihar.com>
2007-05-18 14:18:20 +0000

    Indentation.

--- include/gammu-bitmap.h
+++ include/gammu-bitmap.h
@@ -25,8 +25,8 @@ typedef enum {
 
 typedef struct {
 	GSM_BinaryPicture_Types Type;
-	unsigned char 		*Buffer;
-	int 			Length;
+	unsigned char *Buffer;
+	int Length;
 } GSM_BinaryPicture;
 
 /**
@@ -90,76 +90,76 @@ typedef struct {
 	/**
 	 * For all: bitmap type
 	 */
-	GSM_Bitmap_Types 	Type;
+	GSM_Bitmap_Types Type;
 	/**
 	 * For caller group logos: number of group
 	 * For startup logos: number of animated bitmap
 	 */
-	unsigned char 		Location;
+	unsigned char Location;
 	/**
 	 * For dealer/welcome note text: text
 	 * For caller group logo: name of group
          * For picture images: text assigned to it
 	 */
-	unsigned char 		Text[2 * (GSM_BITMAP_TEXT_LENGTH + 1)];
+	unsigned char Text[2 * (GSM_BITMAP_TEXT_LENGTH + 1)];
 	/**
 	 * For caller group logo: true, when logo is enabled in group
 	 */
-	bool 			BitmapEnabled;
+	bool BitmapEnabled;
 	/**
 	 * For caller group logo: true, when group has default name
 	 */
-	bool 			DefaultName;
+	bool DefaultName;
 	/**
 	 * For caller group logo: true, when group has default bitmap
 	 */
-	bool 			DefaultBitmap;
+	bool DefaultBitmap;
 	/**
 	 * For caller group logo: true, when group has default ringtone
 	 */
-	bool 			DefaultRingtone;
+	bool DefaultRingtone;
 	/**
 	 * For caller group logo: ringtone ID. Phone model specific
 	 */
-	unsigned char 		RingtoneID;
-	bool 			FileSystemRingtone;
+	unsigned char RingtoneID;
+	bool FileSystemRingtone;
 	/**
 	 * For caller group logo: picture ID. Phone model specific
 	 */
-	int	 		PictureID;
-	bool 			FileSystemPicture;
+	int PictureID;
+	bool FileSystemPicture;
 	/**
 	 * For mono bitmaps: body of bitmap
 	 */
-	unsigned char 		BitmapPoints[GSM_BITMAP_SIZE];
+	unsigned char BitmapPoints[GSM_BITMAP_SIZE];
 	/**
 	 * For mono bitmaps: height specified in pixels
 	 */
-	unsigned char 		BitmapHeight;
+	unsigned char BitmapHeight;
 	/**
 	 * For mono bitmaps: width specified in pixels
 	 */
-	unsigned char 		BitmapWidth;
+	unsigned char BitmapWidth;
 	/**
 	 * For operator logos: Network operator code
 	 */
-	char			NetworkCode[7];
+	char NetworkCode[7];
 	/**
 	 * For picture images: number of sender
 	 */
-	unsigned char 		Sender[2 * (GSM_MAX_NUMBER_LENGTH + 1)];
+	unsigned char Sender[2 * (GSM_MAX_NUMBER_LENGTH + 1)];
 	/**
 	 * For colour bitmaps: ID
 	 */
-	unsigned char 		ID;
+	unsigned char ID;
 	/**
 	 * For binary pictures (GIF, BMP, etc.): frame and length
 	 */
-	GSM_BinaryPicture 	BinaryPic;
+	GSM_BinaryPicture BinaryPic;
 	/**
 	 * Bitmap name
 	 */
-	char			*Name;
+	char *Name;
 } GSM_Bitmap;
 
 #define MAX_MULTI_BITMAP 6
@@ -173,11 +173,11 @@ typedef struct {
 	/**
 	 * Number of bitmaps
 	 */
-	unsigned char		Number;
+	unsigned char Number;
 	/**
 	 * All bitmaps
 	 */
-	GSM_Bitmap		Bitmap[MAX_MULTI_BITMAP];
+	GSM_Bitmap Bitmap[MAX_MULTI_BITMAP];
 } GSM_MultiBitmap;
 
 /**
@@ -185,13 +185,13 @@ typedef struct {
  *
  * \ingroup Bitmap
  */
-GSM_Error GSM_GetBitmap(GSM_StateMachine *s, GSM_Bitmap *Bitmap);
+GSM_Error GSM_GetBitmap(GSM_StateMachine * s, GSM_Bitmap * Bitmap);
 /**
  * Sets bitmap in phone.
  *
  * \ingroup Bitmap
  */
-GSM_Error GSM_SetBitmap(GSM_StateMachine *s, GSM_Bitmap *Bitmap);
+GSM_Error GSM_SetBitmap(GSM_StateMachine * s, GSM_Bitmap * Bitmap);
 
 /**
  * Prints bitmap to file.
@@ -201,8 +201,8 @@ GSM_Error GSM_SetBitmap(GSM_StateMachine *s, GSM_Bitmap *Bitmap);
  *
  * \ingroup Bitmap
  */
-void GSM_PrintBitmap(FILE *file, GSM_Bitmap *bitmap);
+void GSM_PrintBitmap(FILE * file, GSM_Bitmap * bitmap);
 
-GSM_Error GSM_SaveBitmapFile	(char *FileName, GSM_MultiBitmap *bitmap);
-GSM_Error GSM_ReadBitmapFile	(char *FileName, GSM_MultiBitmap *bitmap);
+GSM_Error GSM_SaveBitmapFile(char *FileName, GSM_MultiBitmap * bitmap);
+GSM_Error GSM_ReadBitmapFile(char *FileName, GSM_MultiBitmap * bitmap);
 #endif
