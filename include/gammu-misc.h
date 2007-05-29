6892910899d13bfa03216700a4dfc829367ee5ab
Michal Čihař <michal@cihar.com>
2007-05-29 12:41:48 +0000

    Documentation.

--- include/gammu-misc.h
+++ include/gammu-misc.h
@@ -1,15 +1,58 @@
+/**
+ * \file gammu-misc.h
+ * \author Michal Čihař
+ *
+ * Message data and functions.
+ */
+#ifndef __gammu_message_h
+#define __gammu_message_h
+
 #include <stdio.h>
 #include <gammu-file.h>
 
+/**
+ * Reads single line from file.
+ *
+ * \param File File descriptor to read from.
+ * \param Line Buffer where t ostore result.
+ * \param count Maximal length of text which can be stored in buffer.
+ *
+ * \return Length of read line, -1 on error.
+ */
 int GetLine(FILE * File, char *Line, int count);
+
+/**
+ * Reads file over HTTP.
+ *
+ * \param server Server to connect.
+ * \param filename File to grab on that server.
+ * \param file Storage for resulting file.
+ *
+ * \return True on success.
+ */
 bool GSM_ReadHTTPFile(unsigned char *server, unsigned char *filename,
 		      GSM_File * file);
 
+/**
+ * Gets Gammu library version.
+ */
 const char *GetGammuVersion(void);
+
+/**
+ * Gets compiler which was used to compile Gammu library.
+ */
 const char *GetCompiler(void);
+
+/**
+ * Gets host OS.
+ */
 const char *GetOS(void);
 
+/**
+ * Returns path to Gammu locales.
+ */
 const char *GetGammuLocalePath(void);
+
 /**
  * Initializes locales.
  */
@@ -31,4 +74,12 @@ extern void GSM_InitLocales(const char *path);
 #  define my_sleep(x) usleep(x*1000)
 #endif
 
+/**
+ * Encodes text to hexadecimal binary representation.
+ */
 void EncodeHexBin(unsigned char *dest, const unsigned char *src, int len);
+#endif
+
+/* Editor configuration
+ * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
+ */
