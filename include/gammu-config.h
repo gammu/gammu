d8aebeaea6a06c297eed1e99cae58835baf38807
Michal Čihař <michal@cihar.com>
2007-06-01 13:18:33 +0000

    Set version to 1.11.92.

--- include/gammu-config.h
+++ include/gammu-config.h
@@ -6,8 +6,8 @@
 /* -------------------------- Gammu specific ---------------------------- */
 
 /* Version of package */
-#define VERSION "1.11.91"
-#define VERSION_WIN "1,11,91,0"
+#define VERSION "1.11.92"
+#define VERSION_WIN "1,11,92,0"
 
 /* Define if want DEBUG info */
 /* #undef DEBUG */
