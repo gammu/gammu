655b3ae0fbbb50b22b8247aa9962fdd39a55887c
Michal Čihař <michal@cihar.com>
2007-06-12 10:30:30 +0000

    Separate main pages for API and internals.

--- templates/api.desc.cmake
+++ templates/api.desc.cmake
@@ -1,11 +1,11 @@
 /**
- * \mainpage Gammu
+ * \mainpage Gammu API
  * \version @VERSION@
  * \author Michal Čihař
  *
  * \b Gammu is library for communicating with (mostly) GSM phones.
- * You can interface to it by using members of \ref GSM_StateMachine
- * structure.
+ * You can interface to it by creating \ref GSM_StateMachine and then
+ * using various functions on it.
  *
  * This documentation is still not complete, however we try to complete
  * documentation to every code part we touch. If you don't userstand
