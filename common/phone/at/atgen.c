c4a71650d6bc21ee7c669cd48377e30444ba3e35
Michal Čihař <michal@cihar.com>
2007-07-02 14:25:52 +0000

    Avoid warnings.

--- common/phone/at/atgen.c
+++ common/phone/at/atgen.c
@@ -4498,7 +4498,7 @@ GSM_Error ATGEN_PressKey(GSM_StateMachine *s, GSM_KeyCode Key, bool Press)
 	GSM_Error	error;
 	unsigned char 	frame[40];
 	char key[20];
-	size_t len;
+	int len;
 	unsigned char unicode_key[20];
 	GSM_Phone_ATGENData	*Priv = &s->Phone.Data.Priv.ATGEN;
 
