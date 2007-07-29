02e7eba02623c33af24babf29030a0a52b7780f9
Michal Čihař <michal@cihar.com>
2007-07-29 08:58:46 +0000

    Added some compile time testing.

---
+++ tests/statemachine-alloc.c
@@ -0,0 +1,23 @@
+#include <gammu.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+GSM_StateMachine *s;
+
+int main(int argc, char **argv)
+{
+	/* Allocates state machine */
+	s = GSM_AllocStateMachine();
+	if (s == NULL) {
+		printf("Could not allocate state machine!\n");
+		return 1;
+	}
+
+	/* Free state machine */
+	GSM_FreeStateMachine(s);
+	return 0;
+}
+
+/* Editor configuration
+ * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
+ */
