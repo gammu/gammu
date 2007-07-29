c5d007baf9c1377cdb32e636f948ab4ab3c2c145
Michal Čihař <michal@cihar.com>
2007-07-29 12:59:44 +0000

    Implement comparing in test.

--- tests/vcard-read.c
+++ tests/vcard-read.c
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
 	size_t len;
 	bool generate = false;
 	GSM_Backup backup;
+	int i;
 
 	/* Check parameters */
 	if (argc != 3 && argc != 4) {
@@ -79,6 +80,73 @@ int main(int argc, char **argv)
 		printf("Error reading backup from %s\n", argv[2]);
 		return 1;
 	}
+
+	/* Compare size */
+	if (pbk.EntriesNum != backup.PhonePhonebook[0]->EntriesNum) {
+		printf("Different number of entries!\n");
+		return 1;
+	}
+
+	/* Compare content */
+	for (i = 0; i < pbk.EntriesNum; i++) {
+		switch (pbk.Entries[i].EntryType) {
+			case PBK_Number_General     :
+			case PBK_Number_Mobile      :
+			case PBK_Number_Work        :
+			case PBK_Number_Fax         :
+			case PBK_Number_Home        :
+			case PBK_Number_Pager       :
+			case PBK_Number_Other       :
+			case PBK_Text_Note          :
+			case PBK_Text_Postal        :
+			case PBK_Text_WorkPostal:
+			case PBK_Text_Email         :
+			case PBK_Text_Email2        :
+			case PBK_Text_URL           :
+			case PBK_Text_LUID          :
+			case PBK_Text_Name          :
+			case PBK_Text_LastName      :
+			case PBK_Text_FirstName     :
+			case PBK_Text_FormalName    :
+			case PBK_Text_NickName      :
+			case PBK_Text_Company       :
+			case PBK_Text_JobTitle      :
+			case PBK_Text_StreetAddress :
+			case PBK_Text_City          :
+			case PBK_Text_State         :
+			case PBK_Text_Zip           :
+			case PBK_Text_Country       :
+			case PBK_Text_WorkStreetAddress :
+			case PBK_Text_WorkCity          :
+			case PBK_Text_WorkState         :
+			case PBK_Text_WorkZip           :
+			case PBK_Text_WorkCountry       :
+			case PBK_Text_Custom1       :
+			case PBK_Text_Custom2       :
+			case PBK_Text_Custom3       :
+			case PBK_Text_Custom4       :
+			case PBK_Text_UserID:
+			case PBK_Text_PictureName:
+			case PBK_Caller_Group       :
+				if (!mywstrncmp(
+					pbk.Entries[i].Text,
+					backup.PhonePhonebook[0]->Entries[i].Text,
+					0)) {
+					printf("Field %d is not the same!\n", i);
+					return 1;
+				}
+				break;
+			case PBK_Date:
+			case PBK_Category:
+			case PBK_Private:
+			case PBK_RingtoneID:
+			case PBK_PictureID:
+			case PBK_CallLength:
+			case PBK_LastModified:
+				break;
+		}
+	}
+
 
 	/* Free data */
 	GSM_FreeBackup(&backup);
