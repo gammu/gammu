8033cb9e845f96db3006a3d66e69913a89c2bbc1
Michal Čihař <michal@cihar.com>
2007-07-31 13:44:14 +0000

    Move some commom code out of gammu.c.

---
+++ gammu/common.h
@@ -0,0 +1,52 @@
+#include <gammu.h>
+
+extern volatile bool 			gshutdown;
+extern GSM_StateMachine		*s;
+extern INI_Section		*cfg;
+extern GSM_Error			error;
+extern bool 				phonedb;
+extern bool				batch;
+extern bool				batchConn;
+extern bool always_answer_yes;
+
+const char *GetMonthName(const int month);
+const char *GetDayName(const int day);
+void	Print_Error		(GSM_Error error);
+void	GSM_Init		(bool checkerror);
+void	GSM_Terminate		(void);
+void GetStartStop(int *start, int *stop, int num, int argc, char *argv[]);
+bool answer_yes(const char *text);
+
+/**
+ * Prints information about security status.
+ */
+void PrintSecurityStatus();
+
+GSM_MemoryType MemoryTypeFromString(const char *type);
+
+/**
+ * Write error to user.
+ */
+PRINTF_STYLE(1, 2)
+int printf_err(const char *format, ...);
+
+/**
+ * Write warning to user.
+ */
+PRINTF_STYLE(1, 2)
+int printf_warn(const char *format, ...);
+
+int printmsgerr(char *format, ...);
+
+/**
+ * Interrupt handler, sets shutdown flag.
+ */
+void interrupt(int sign);
+
+#ifdef GSM_ENABLE_BEEP
+void	GSM_PhoneBeep		(void);
+#endif
+
+/* How should editor hadle tabs in this file? Add editor commands here.
+ * vim: noexpandtab sw=8 ts=8 sts=8:
+ */
---
+++ gammu/formats.h
@@ -0,0 +1,7 @@
+#define LISTFORMAT "%-20s : "
+#define PRINTSECONDS(num) printf(ngettext("%d second", "%d seconds", num), num);
+#define PRINTMINUTES(num) printf(ngettext("%d minute", "%d minutes", num), num);
+#define PRINTHOURS(num) printf(ngettext("%d hour", "%d hours", num), num);
+#define PRINTDAYS(num) printf(ngettext("%d day", "%d days", num), num);
+#define PRINTWEEKS(num) printf(ngettext("%d week", "%d weeks", num), num);
+#define PRINTYEARS(num) printf(ngettext("%d year", "%d years", num), num);
