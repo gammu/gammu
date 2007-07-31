10aa89fa5dd73927fd4a944d1d8981f7f351d459
Michal Čihař <michal@cihar.com>
2007-07-31 14:43:49 +0000

    Add missing headers.

---
+++ gammu/memory.h
@@ -0,0 +1,9 @@
+#include <gammu.h>
+
+extern void DeleteMemory(int argc, char *argv[]);
+extern void GetMemory(int argc, char *argv[]);
+extern void GetAllMemory(int argc, char *argv[]);
+extern void SearchMemory(int argc, char *argv[]);
+extern void ListMemoryCategory(int argc, char *argv[]);
+extern void PrintMemoryEntry(GSM_MemoryEntry * entry);
+extern void PrintMemorySubEntry(GSM_SubMemoryEntry * entry);
