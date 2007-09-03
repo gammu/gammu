025ac1ff624315986754aac287f956deda17dc97
Michal Čihař <michal@cihar.com>
2007-09-03 01:35:31 +0000

    Blind attempt to make bluetooth check work on freebsd.

--- cmake/FindBluez.cmake
+++ cmake/FindBluez.cmake
@@ -6,8 +6,7 @@
 #
 # Copyright (c) 2007, Michal Cihar, <michal@cihar.com>
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# vim: expandtab sw=4 ts=4 sts=4:
 
 if (NOT DEFINED BLUEZ_FOUND)
     if (CROSS_MINGW)
@@ -16,30 +15,32 @@ if (NOT DEFINED BLUEZ_FOUND)
        set(BLUEZ_LIBRARIES ws2_32)
        message(STATUS "Using Windows native Bluetooth: ${BLUEZ_INCLUDE_DIR}, ${BLUEZ_LIBRARIES}")
     endif (CROSS_MINGW)
-	if (NOT BLUEZ_FOUND)
+
+    if (NOT BLUEZ_FOUND)
         pkg_check_modules (BLUEZ bluez)
-	endif (NOT BLUEZ_FOUND)
+    endif (NOT BLUEZ_FOUND)
 
-	if (NOT BLUEZ_FOUND)
-		find_path(BLUEZ_INCLUDE_DIR bluetooth/bluetooth.h
-		   /usr/include
-		   /usr/local/include
-		)
+    if (NOT BLUEZ_FOUND)
+        find_path(BLUEZ_INCLUDE_DIR NAMES bluetooth/bluetooth.h bluetooth.h
+           PATHS
+           /usr/include
+           /usr/local/include
+        )
 
-		find_library(BLUEZ_LIBRARIES NAMES bluetooth
-		   PATHS
-		   /usr/lib
-		   /usr/local/lib
-		)
+        find_library(BLUEZ_LIBRARIES NAMES bluetooth
+           PATHS
+           /usr/lib
+           /usr/local/lib
+        )
 
-		if(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
-		   set(BLUEZ_FOUND TRUE CACHE INTERNAL "Bluez found")
-		   message(STATUS "Found Bluez: ${BLUEZ_INCLUDE_DIR}, ${BLUEZ_LIBRARIES}")
-		else(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
-		   set(BLUEZ_FOUND FALSE CACHE INTERNAL "Bluez found")
-		   message(STATUS "Bluez not found.")
-		endif(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
+        if(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
+           set(BLUEZ_FOUND TRUE CACHE INTERNAL "Bluez found")
+           message(STATUS "Found Bluez: ${BLUEZ_INCLUDE_DIR}, ${BLUEZ_LIBRARIES}")
+        else(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
+           set(BLUEZ_FOUND FALSE CACHE INTERNAL "Bluez found")
+           message(STATUS "Bluez not found.")
+        endif(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
 
-		mark_as_advanced(BLUEZ_INCLUDE_DIR BLUEZ_LIBRARIES)
-	endif (NOT BLUEZ_FOUND)
+        mark_as_advanced(BLUEZ_INCLUDE_DIR BLUEZ_LIBRARIES)
+    endif (NOT BLUEZ_FOUND)
 endif (NOT DEFINED BLUEZ_FOUND)
