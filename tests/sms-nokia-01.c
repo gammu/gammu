b390c64a4169140a606fb492431531f104992c26
Michal Čihař <michal@cihar.com>
2007-09-08 01:18:18 +0000

    Add testcase for decoding SMS frame on Nokia.

---
+++ tests/sms-nokia-01.c
@@ -0,0 +1,59 @@
+/* Test for decoding SMS on Nokia 6110 driver */
+
+#include <gammu.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../common/protocol/protocol.h" /* Needed for GSM_Protocol_Message */
+#include "../common/gsmstate.h" /* Needed for state machine internals */
+
+GSM_StateMachine *s;
+GSM_Protocol_Message msg;
+unsigned char data[] = {
+	0x01, 0x08, 0x00, 0x08, 0x01, 0x02, 0x01, 0x00, 0x05, 0x91, 0x26, 0x18, 0x16, 0x42, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x08, 0x0D, 0x91, 0x26, 0x58, 0x26, 0x84, 0x88, 0x65,
+	0xF8, 0x00, 0x00, 0x00, 0x70, 0x90, 0x30, 0x10, 0x61, 0x63, 0x82, 0xD4, 0xF2, 0x9C, 0x0E, 0x9A,
+	0xB7, 0xE7
+	};
+GSM_Error error;
+GSM_MultiSMSMessage sms;
+GSM_MultiPartSMSInfo	SMSInfo;
+
+/* This is not part of API! */
+extern GSM_Error N6110_ReplyGetSMSMessage(GSM_Protocol_Message msg, GSM_StateMachine *s);
+
+int main(int argc, char **argv)
+{
+	GSM_Debug_Info *di;
+
+	di = GSM_GetGlobalDebug();
+	GSM_SetDebugFileDescriptor(stderr, di);
+	GSM_SetDebugLevel("textall", di);
+
+	/* Allocates state machine */
+	s = GSM_AllocStateMachine();
+	if (s == NULL) {
+		printf("Could not allocate state machine!\n");
+		return 1;
+	}
+
+	/* Init message */
+	msg.Type = 0x14;
+	msg.Length = 0x32;
+	msg.Buffer = data;
+
+	s->Phone.Data.GetSMSMessage = &sms;
+
+	/* Parse it */
+	error = N6110_ReplyGetSMSMessage(msg, s);
+
+	/* Free state machine */
+	GSM_FreeStateMachine(s);
+
+	printf("%s\n", GSM_ErrorString(error));
+
+	return (error == ERR_NONE) ? 0 : 1;
+}
+
+/* Editor configuration
+ * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
+ */
