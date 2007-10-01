c94ebe3e27e9a9e5a632d8c1e5a0f65ba0cbc133
Michal Čihař <michal@cihar.com>
2007-10-01 01:26:57 +0000

    Convert specific -nodtr connection to generic -nodtr suffix.

--- common/protocol/nokia/fbus2.c
+++ common/protocol/nokia/fbus2.c
@@ -264,13 +264,9 @@ static GSM_Error FBUS2_StateMachine(GSM_StateMachine *s, unsigned char rx_char)
 			case GCT_FBUS2:
 			case GCT_FBUS2DLR3:
 			case GCT_DKU5FBUS2:
-			case GCT_ARK3116FBUS2:
 			case GCT_FBUS2PL2303:
 			case GCT_FBUS2BLUE:
 			case GCT_BLUEFBUS2:
-			case GCT_DKU5FBUS2NODTR:
-			case GCT_FBUS2NODTR:
-			case GCT_FBUS2DLR3NODTR:
 				if (rx_char == FBUS2_FRAME_ID) correct = true;
 				break;
 			case GCT_FBUS2IRDA:
@@ -395,35 +391,22 @@ static GSM_Error FBUS2_Initialise(GSM_StateMachine *s)
 	case GCT_DKU5FBUS2:
 	case GCT_FBUS2PL2303:
 	case GCT_FBUS2DLR3:
-	case GCT_ARK3116FBUS2:
-	case GCT_FBUS2DLR3NODTR:
-	case GCT_DKU5FBUS2NODTR:
-		if (s->ConnectionType != GCT_ARK3116FBUS2 &&
-				s->ConnectionType != GCT_DKU5FBUS2NODTR &&
-				s->ConnectionType != GCT_FBUS2DLR3NODTR
-				) {
-			error = Device->DeviceSetDtrRts(s,false,false);
-			if (error != ERR_NONE) return error;
-			my_sleep(1000);
+		error = Device->DeviceSetDtrRts(s,false,false);
+		if (error != ERR_NONE) return error;
+		my_sleep(1000);
 
-			error = Device->DeviceSetDtrRts(s,true,true);
-			if (error != ERR_NONE) return error;
-			my_sleep(1000);
+		error = Device->DeviceSetDtrRts(s,true,true);
+		if (error != ERR_NONE) return error;
+		my_sleep(1000);
 
-			error = Device->DeviceSetSpeed(s,19200);
-			if (error != ERR_NONE) return error;
-		}
+		error = Device->DeviceSetSpeed(s,19200);
+		if (error != ERR_NONE) return error;
 
 		error = FBUS2_ATSwitch(s);
 		if (error != ERR_NONE) return error;
 
-		if (s->ConnectionType != GCT_ARK3116FBUS2 &&
-				s->ConnectionType != GCT_DKU5FBUS2NODTR &&
-				s->ConnectionType != GCT_FBUS2DLR3NODTR
-				) {
-			error = Device->DeviceSetSpeed(s,115200);
-			if (error != ERR_NONE) return error;
-		}
+		error = Device->DeviceSetSpeed(s,115200);
+		if (error != ERR_NONE) return error;
 
 		error = FBUS2_InitSequence(s);
 		if (error != ERR_NONE) return error;
@@ -431,14 +414,11 @@ static GSM_Error FBUS2_Initialise(GSM_StateMachine *s)
 		break;
 #endif
 	case GCT_FBUS2:
-	case GCT_FBUS2NODTR:
-		if (s->ConnectionType != GCT_FBUS2NODTR) {
-			error = Device->DeviceSetSpeed(s,115200);
-			if (error != ERR_NONE) return error;
+		error = Device->DeviceSetSpeed(s,115200);
+		if (error != ERR_NONE) return error;
 
-			error = Device->DeviceSetDtrRts(s,true,false); /*DTR high,RTS low*/
-			if (error != ERR_NONE) return error;
-		}
+		error = Device->DeviceSetDtrRts(s,true,false); /*DTR high,RTS low*/
+		if (error != ERR_NONE) return error;
 
 		error = FBUS2_InitSequence(s);
 		if (error != ERR_NONE) return error;
