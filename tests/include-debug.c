5ff10561f17f788a70a8de6abbd4c5322619dea6
Michal Čihař <michal@cihar.com>
2007-10-04 04:36:02 +0000

    Add tests for testing whether header files are correct.

---
+++ tests/include-debug.c
@@ -0,0 +1,16 @@
+/* Automatically generated test for validating header file gammu-debug.h */
+#include <gammu-debug.h>
+#include <gammu-debug.h>
+
+/* We do not want to push another header, so we need to copy definiton of UNUSED */
+#ifndef UNUSED
+# if __GNUC__
+#  define UNUSED __attribute__ ((unused))
+# else
+#  define UNUSED
+# endif
+#endif
+
+int main(int argc UNUSED, char** argv UNUSED) {
+	return 0;
+}
