f33dc04943253edd2350fa4a91b5add29af54ae5
Michal Čihař <michal@cihar.com>
2007-10-18 07:48:12 +0000

    - Make file format configurable through API. - This is needed for later support of UTF-8 backups. - Done now because we're breaking API anyway.

--- include/gammu-backup.h
+++ include/gammu-backup.h
@@ -285,18 +285,76 @@ typedef struct {
 } GSM_Backup;
 
 /**
+ * Backup data.
+ *
+ * \ingroup Backup
+ */
+typedef enum {
+	/**
+	 * Compatibility with old bool used instead of format.
+	 *
+	 * File type is guessed for extension, non unicode format used
+	 * for Gammu backup.
+	 */
+	GSM_Backup_Auto = 0,
+	/**
+	 * Compatibility with old bool used instead of format.
+	 *
+	 * File type is guessed for extension, unicode format used
+	 * for Gammu backup.
+	 */
+	GSM_Backup_AutoUnicode = 1,
+	/**
+	 * LMB format, compatible with Logo manager, can store
+	 * phonebooks and logos.
+	 */
+	GSM_Backup_LMB,
+	/**
+	 * vCalendar standard, can store todo and calendar entries.
+	 */
+	GSM_Backup_VCalendar,
+	/**
+	 * vCard standard, can store phone phonebook entries.
+	 */
+	GSM_Backup_VCard,
+	/**
+	 * LDIF (LDAP Data Interchange Format), can store phone
+	 * phonebook entries.
+	 */
+	GSM_Backup_LDIF,
+	/**
+	 * iCalendar standard, can store todo and calendar entries.
+	 */
+	GSM_Backup_ICS,
+	/**
+	 * Gammu own format can store almost anything from phone.
+	 *
+	 * This is ASCII version of the format, Unicode strings are HEX
+	 * encoded.
+	 */
+	GSM_Backup_Gammu,
+	/**
+	 * Gammu own format can store almost anything from phone.
+	 *
+	 * This is UCS2-BE version of the format.
+	 */
+	GSM_Backup_GammuUCS2,
+} GSM_BackupFormat;
+
+/**
  * Save backup file.
  *
  * \ingroup Backup
  *
  * \param FileName Name of file (format is detected from it).
- * \param backup structure holding backup data
- * \param UseUnicode Whether to use unicode (if available).
+ * \param Backup structure holding backup data
+ * \param Format Backup format.
  *
  * \return Error code
  */
-GSM_Error GSM_SaveBackupFile(char *FileName, GSM_Backup * backup,
-			     bool UseUnicode);
+GSM_Error GSM_SaveBackupFile(char *FileName, GSM_Backup * Backup,
+			     GSM_BackupFormat Format);
+
 /**
  * Reads data from backup file.
  *
