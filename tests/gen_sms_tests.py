a2f1c98639440531c0a4413d9670429ad2456aa2
Michal Čihař <michal@cihar.com>
2007-10-30 05:53:17 +0000

    Added testing of SMS PDU encoding.

---
+++ tests/gen_sms_tests.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+'''
+Gammu SMS backup generator.
+'''
+__author__ = 'Michal Čihař'
+__email__ = 'michal@cihar.com'
+__license__ = '''
+Copyright (c) 2003 - 2007 Michal Čihař
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+
+import os
+
+# Work in both common location when this can be executed:
+try:
+    os.chdir('tests/at-sms-encode/')
+except OSError:
+    os.chdir('at-sms-encode/')
+
+# Numbers we're going to test
+NUMBERS = [
+    '800123456',
+    '+420800123456',
+    '+41761234567',
+    ]
+
+# Text parts we're going to test
+TEXTS = [
+    '123456',
+    'Zkouška sirén',
+    'This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation.',
+    ]
+
+TEMPLATE = '''
+[SMSBackup000]
+SMSC = "+420603052000"
+Sent = 20070605T135630
+State = Read
+Number = "%s"
+Coding = Default
+'''
+
+def write_text(f, text):
+    '''
+    Writes text splitted and encoded in same way as Gammu does it for SMS backups.
+    '''
+    encoded = text.encode('UTF-16-BE').encode('HEX')
+    line = 0
+    while len(encoded) > 0:
+        f.write('Text%02d = %s\n' % (line, encoded[:200]))
+        encoded = encoded[200:]
+        line = line + 1
+
+def generate():
+    '''
+    Generates test data based on NUMBERS and TEXTS variables.
+    '''
+    index = 1
+    for num in range(len(NUMBERS)):
+        for text in range(len(TEXTS)):
+            f = file('%02d.backup' % index, 'w')
+            f.write(TEMPLATE % NUMBERS[num])
+            write_text(f, TEXTS[text])
+            f.close()
+            index = index + 1
+
+if __name__ == '__main__':
+    generate()
