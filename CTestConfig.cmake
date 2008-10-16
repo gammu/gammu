3328970e8f2b5b7ae34d80cdba2dfda0fe401ff9
Michal Čihař <michal@cihar.com>
2008-10-16 13:02:04 +0000

    Use own cdash instance.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "Gammu")
 set(CTEST_NIGHTLY_START_TIME "03:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDashPublic/submit.php?project=Gammu")
+set(CTEST_DROP_SITE "cdash.cihar.com")
+set(CTEST_DROP_LOCATION "/submit.php?project=Gammu")
 set(CTEST_DROP_SITE_CDASH TRUE)
