87d104a25a0c35b8e6675a70960972dc73ae9a34
Michal Čihař <michal@cihar.com>
2008-10-24 13:14:24 +0000

    Disable OBEX with Motorola Z6c, it is broken (bug #746).

--- common/gsmphones.c
+++ common/gsmphones.c
@@ -708,6 +708,7 @@ OnePhoneModel allmodels[] = {
 	{"V635",	"V635",	"",					   {F_OBEX, F_IRMC_LEVEL_2, F_MODE22, F_PBK_UNICODE, 0}},
 	{"L7",		"L7", "",					   {F_CKPD_NO_UNICODE, 0}},
 	{"K1v",		"K1v", "",					   {F_NO_ATOBEX, F_PBK_UNICODE, 0}},
+	{"Z6c",		"Z6c", "",					   {F_NO_ATOBEX, F_PBK_UNICODE, 0}},
 	{"RAZRV3x",	"RAZRV3x",	"",				   {F_OBEX, F_IRMC_LEVEL_2, F_MODE22, F_PBK_UNICODE, 0}},
 
 	/* Sharp */
