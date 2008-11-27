1e2495575d9acc0494ef70b9c097e93c20b5c79d
Michal Čihař <michal@cihar.com>
2008-11-27 08:49:02 +0000

    Simplify condition, rest is up to compiler.

--- common/misc/coding/coding.c
+++ common/misc/coding/coding.c
@@ -1166,13 +1166,7 @@ int strncasecmp (const char *s1, const char *s2, size_t n)
 		++p2;
 	} while (c1 == c2);
 
-	if (UCHAR_MAX <= INT_MAX)
-		return c1 - c2;
-	else
-	/* On machines where 'char' and 'int' are types of the same size, the
-	difference of two 'unsigned char' values - including the sign bit -
-	doesn't fit in an 'int'.  */
-		return (c1 > c2 ? 1 : c1 < c2 ? -1 : 0);
+	return (c1 > c2 ? 1 : c1 < c2 ? -1 : 0);
 }
 #undef TOLOWER
 #endif
