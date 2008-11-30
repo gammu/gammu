fa43d75b934d08e439e8d1519218ca0cee43a73b
Michal Čihař <michal@cihar.com>
2008-11-30 10:56:09 +0000

    Cleanup.

--- common/misc/coding/coding.c
+++ common/misc/coding/coding.c
@@ -1960,4 +1960,3 @@ bool IconvEncode(const char *charset, const unsigned char *input, const size_t i
 /* How should editor hadle tabs in this file? Add editor commands here.
  * vim: noexpandtab sw=8 ts=8 sts=8:
  */
-
