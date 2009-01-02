3dd22b8535cc62b7e5911646afaa7bffecf4dc27
Michal Čihař <michal@cihar.com>
2009-01-02 11:00:39 +0000

    Prepare separate SMSD main loop.

--- smsd/gammu-smsd.h
+++ smsd/gammu-smsd.h
@@ -9,8 +9,16 @@
 
 #include <gammu-error.h>
 #include <gammu-message.h>
+#include <gammu-misc.h>
+
+typedef struct _GSM_SMSDConfig GSM_SMSDConfig;
 
 void      SMSDaemon		(int argc, char *argv[]);
 GSM_Error SMSDaemonSendSMS	(char *service, char *filename, GSM_MultiSMSMessage *sms);
+
+/* GSM_SMSDConfig *Config, */
+PRINTF_STYLE(1, 2)
+void WriteSMSDLog(const char *format, ...);
+
 
 #endif
