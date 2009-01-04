2beabe7992c2d3e8a762f48f6b932be7cbff7576
Michal Čihař <michal@cihar.com>
2009-01-04 22:43:43 +0000

    Add missing test for smsd header file.

---
+++ tests/include-smsd.c
@@ -0,0 +1,18 @@
+/* Automatically generated test for validating header file gammu-smsd.h */
+/* See gen-include-test.sh for details */
+
+#include <gammu-smsd.h>
+#include <gammu-smsd.h>
+
+/* We do not want to push another header, so we need to copy definiton of UNUSED */
+#ifndef UNUSED
+# if __GNUC__
+#  define UNUSED __attribute__ ((unused))
+# else
+#  define UNUSED
+# endif
+#endif
+
+int main(int argc UNUSED, char** argv UNUSED) {
+	return 0;
+}
