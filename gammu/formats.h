bdd07f70ed7c6cfc8703dc761dd6d00f2da8366b
Michal Čihař <michal@cihar.com>
2009-01-05 15:26:22 +0000

    Forgotten file.

---
+++ gammu/formats.h
@@ -0,0 +1,10 @@
+#ifndef __helper_formats_h__
+#define __helper_formats_h__
+#define LISTFORMAT "%-20s : "
+#define PRINTSECONDS(num) printf(ngettext("%d second", "%d seconds", num), num);
+#define PRINTMINUTES(num) printf(ngettext("%d minute", "%d minutes", num), num);
+#define PRINTHOURS(num) printf(ngettext("%d hour", "%d hours", num), num);
+#define PRINTDAYS(num) printf(ngettext("%d day", "%d days", num), num);
+#define PRINTWEEKS(num) printf(ngettext("%d week", "%d weeks", num), num);
+#define PRINTYEARS(num) printf(ngettext("%d year", "%d years", num), num);
+#endif
