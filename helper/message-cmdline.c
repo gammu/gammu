a1bfdfe98f0d4164f9d5d09a03a49ea93501fefc
Michal Čihař <michal@cihar.com>
2009-01-12 15:39:39 +0000

    Check whether backup contains data...

--- helper/message-cmdline.c
+++ helper/message-cmdline.c
@@ -394,7 +394,7 @@ GSM_Error CreateMessage(GSM_Message_Type *type, GSM_MultiSMSMessage *sms, int ar
 			if (i == atoi(argv[1 + startarg])-1) break;
 			i++;
 		}
-		if (i != atoi(argv[1 + startarg])-1) {
+		if (i != atoi(argv[1 + startarg])-1 || Backup.WAPSettings[i] == NULL) {
 			printf("%s\n", _("WAP settings not found in file"));
 			exit(-1);
 		}
