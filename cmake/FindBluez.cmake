cab7c877a2cf8a28f843d87e8f99ee27573c0b40
Michal Čihař <michal@cihar.com>
2009-01-29 14:04:07 +0000

    Cleanup (remove two same ifs).

--- cmake/FindBluez.cmake
+++ cmake/FindBluez.cmake
@@ -9,31 +9,29 @@
 # vim: expandtab sw=4 ts=4 sts=4:
 
 if (NOT BLUEZ_FOUND)
-    if (NOT BLUEZ_FOUND)
-        pkg_check_modules (BLUEZ_PKG bluez)
+    pkg_check_modules (BLUEZ_PKG bluez)
 
-        find_path(BLUEZ_INCLUDE_DIR NAMES bluetooth/bluetooth.h
-           PATHS
-           ${BLUEZ_PKG_INCLUDEDIR}
-           /usr/include
-           /usr/local/include
-        )
+    find_path(BLUEZ_INCLUDE_DIR NAMES bluetooth/bluetooth.h
+       PATHS
+       ${BLUEZ_PKG_INCLUDEDIR}
+       /usr/include
+       /usr/local/include
+    )
 
-        find_library(BLUEZ_LIBRARIES NAMES bluetooth
-           PATHS
-           ${BLUEZ_PKG_LIBDIR}
-           /usr/lib
-           /usr/local/lib
-        )
+    find_library(BLUEZ_LIBRARIES NAMES bluetooth
+       PATHS
+       ${BLUEZ_PKG_LIBDIR}
+       /usr/lib
+       /usr/local/lib
+    )
 
-        if(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
-           set(BLUEZ_FOUND TRUE CACHE INTERNAL "Bluez found")
-           message(STATUS "Found Bluez: ${BLUEZ_INCLUDE_DIR}, ${BLUEZ_LIBRARIES}")
-        else(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
-           set(BLUEZ_FOUND FALSE CACHE INTERNAL "Bluez found")
-           message(STATUS "Bluez not found.")
-        endif(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
+    if(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
+       set(BLUEZ_FOUND TRUE CACHE INTERNAL "Bluez found")
+       message(STATUS "Found Bluez: ${BLUEZ_INCLUDE_DIR}, ${BLUEZ_LIBRARIES}")
+    else(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
+       set(BLUEZ_FOUND FALSE CACHE INTERNAL "Bluez found")
+       message(STATUS "Bluez not found.")
+    endif(BLUEZ_INCLUDE_DIR AND BLUEZ_LIBRARIES)
 
-        mark_as_advanced(BLUEZ_INCLUDE_DIR BLUEZ_LIBRARIES)
-    endif (NOT BLUEZ_FOUND)
+    mark_as_advanced(BLUEZ_INCLUDE_DIR BLUEZ_LIBRARIES)
 endif (NOT BLUEZ_FOUND)
