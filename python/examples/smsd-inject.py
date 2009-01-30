e93399f17b6d7a172f1a0856613df011ddcb029c
Michal Čihař <michal@cihar.com>
2009-01-30 15:35:07 +0000

    Example for SMSD injecting from python.

---
+++ python/examples/smsd-inject.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# sample script to show how to send SMS
+
+import gammu
+
+smsd = gammu.SMSD('/etc/gammu-smsdrc')
+
+import sys
+if len(sys.argv) != 2:
+    print 'This requires one parameter containing number!'
+    sys.exit(1)
+
+message = {'Text': 'python-gammu testing message', 'SMSC': {'Location': 1}, 'Number': sys.argv[1]}
+
+smsd.InjectSMS([message])
