d37fb789f699baa731fccaab9705c99138223877
Michal Čihař <michal@cihar.com>
2009-02-01 10:19:18 +0000

    Documentation.

--- include/gammu-smsd.h
+++ include/gammu-smsd.h
@@ -22,6 +22,9 @@
  */
 typedef struct _GSM_SMSDConfig GSM_SMSDConfig;
 
+/**
+ * Length of texts in GSM_SMSDStatus structure.
+ */
 #define SMSD_TEXT_LENGTH 100
 
 /**
@@ -92,17 +95,60 @@ GSM_Error SMSD_GetStatus(GSM_SMSDConfig *Config, GSM_SMSDStatus *status);
  * Flags SMSD daemon to terminate itself gracefully.
  *
  * \param Config Pointer to SMSD configuration data.
+ *
+ * \return Error code
  */
 GSM_Error SMSD_Shutdown(GSM_SMSDConfig *Config);
 
+/**
+ * Reads SMSD configuration.
+ *
+ * \param filename File name of configuration.
+ * \param Config Pointer to SMSD configuration data.
+ * \param uselog Whether to log errors to configured log.
+ *
+ * \return Error code
+ */
 GSM_Error SMSD_ReadConfig(const char *filename, GSM_SMSDConfig *Config, bool uselog);
 
+/**
+ * Terminates SMSD with logging error messages to log. This does not
+ * signal running SMSD to stop, it can be called from initialization of
+ * SMSD wrapping program to terminate with logging.
+ *
+ * \param Config Pointer to SMSD configuration data.
+ * \param msg Message to display.
+ * \param error GSM error code, if applicable.
+ * \param rc Program return code, will be passed to exit (if enabled).
+ */
 void SMSD_Terminate(GSM_SMSDConfig *Config, const char *msg, GSM_Error error, bool exitprogram, int rc);
 
+/**
+ * Main SMS daemon loop. It connects to phone, scans for messages and
+ * sends messages from inbox. Can be interrupted by SMSD_Shutdown.
+ *
+ * \see SMSD_Shutdown
+ *
+ * \param Config Pointer to SMSD configuration data.
+ * \param exit_on_failure Whether failure should lead to terminaton of
+ * program.
+ *
+ * \return Error code
+ */
 GSM_Error SMSD_MainLoop(GSM_SMSDConfig *Config, bool exit_on_failure);
 
+/**
+ * Creates new SMSD configuration.
+ *
+ * \return Pointer to SMSD configuration data block.
+ */
 GSM_SMSDConfig *SMSD_NewConfig(void);
 
+/**
+ * Frees SMSD configuration.
+ *
+ * \param Config Pointer to SMSD configuration data.
+ */
 void SMSD_FreeConfig(GSM_SMSDConfig *config);
 #endif
 
