df1fae35a3677997c109cb6a9ccc8bc9ae573575
Michal Čihař <michal@cihar.com>
2009-02-02 14:26:35 +0000

    More complex SMSD testing. - inject messages to dummy backend - actually check whether messages have been processed

--- smsd/test-smsd.sh
+++ smsd/test-smsd.sh
@@ -26,25 +26,46 @@ model = dummy
 connection = none
 port = @CMAKE_CURRENT_BINARY_DIR@/smsd-test/gammu-dummy
 gammuloc = /dev/null
+loglevel = textall
 
 [smsd]
 service = dbi
 driver = sqlite3
 database = smsd.db
+commtimeout = 1
 dbdir = @CMAKE_CURRENT_BINARY_DIR@/smsd-test/
 debuglevel = 255
 logfile = stderr
 EOT
 
 CONFIG_PATH="@CMAKE_CURRENT_BINARY_DIR@/smsd-test/.smsdrc"
+DUMMY_PATH="@CMAKE_CURRENT_BINARY_DIR@/smsd-test/gammu-dummy"
+
+mkdir -p $sms $DUMMY_PATH/sms/1
+mkdir -p $sms $DUMMY_PATH/sms/2
+mkdir -p $sms $DUMMY_PATH/sms/3
+mkdir -p $sms $DUMMY_PATH/sms/4
+mkdir -p $sms $DUMMY_PATH/sms/5
 
 @CMAKE_CURRENT_BINARY_DIR@/gammu-smsd -c "$CONFIG_PATH" &
 SMSD_PID=$!
 
-sleep 2
+sleep 3
 
-@CMAKE_CURRENT_BINARY_DIR@/gammu-smsd-monitor -c "$CONFIG_PATH" -l 1 -d 0
-@CMAKE_CURRENT_BINARY_DIR@/gammu-smsd-inject -c "$CONFIG_PATH" TEXT 123465 -text "Lorem ipsum."
+for sms in @CMAKE_CURRENT_SOURCE_DIR@/../tests/at-sms-encode/0[1-9].backup ; do
+    number=`echo $sms | @SED_BIN@ 's@.*/0*\([1-9][0-9]*\).backup@\1@'`
+    cp $sms $DUMMY_PATH/sms/1/$number
+done
 
 sleep 5
-kill $SMSD_PID
+
+@CMAKE_CURRENT_BINARY_DIR@/gammu-smsd-inject -c "$CONFIG_PATH" TEXT 123465 -text "Lorem ipsum."
+
+for sms in @CMAKE_CURRENT_SOURCE_DIR@/../tests/at-sms-encode/0[1-9].backup ; do
+    number=`echo $sms | @SED_BIN@ 's@.*/0*\([1-9][0-9]*\).backup@\1@'`
+    cp $sms $DUMMY_PATH/sms/3/$number
+done
+
+sleep 5
+@CMAKE_CURRENT_BINARY_DIR@/gammu-smsd-monitor -c "$CONFIG_PATH" -l 1 -d 0
+sleep 1
