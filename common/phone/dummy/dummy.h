dbdb1b8c5241aaaff312915caf0f8dc47c84f22a
Michal Čihař <michal@cihar.com>
2009-02-02 08:10:42 +0000

    Add forgotten header.

---
+++ common/phone/dummy/dummy.h
@@ -0,0 +1,29 @@
+/* Copyright (c) 2009 by Michal Čihař */
+
+/**
+ * \file dummy.h
+ * @author Michal Čihař
+ */
+/**
+ * @addtogroup Phone
+ * @{
+ */
+/**
+ * @addtogroup DummyPhone
+ * @{
+ */
+#ifndef __phone_dummy_h__
+#define __phone_dummy_h__
+
+#include <stdio.h>
+
+
+typedef struct {
+	FILE *log_file;
+} GSM_Phone_DUMMYData;
+
+#endif
+
+/* How should editor hadle tabs in this file? Add editor commands here.
+ * vim: noexpandtab sw=8 ts=8 sts=8:
+ */
