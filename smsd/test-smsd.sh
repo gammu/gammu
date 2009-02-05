9942e854f5adba14f8fa144ee0ff00ecb9f2bc19
Michal Čihař <michal@cihar.com>
2009-02-05 15:34:31 +0000

    Add option to test all database smsd services.

--- smsd/test-smsd.sh
+++ smsd/test-smsd.sh
@@ -4,7 +4,10 @@ set -x
 set -e
 SMSD_PID=0
 
-echo "This test is quite tricky about timing, if you run it on really slow platform, it might fail."
+SERVICE="$1"
+
+echo "NOTICE: This test is quite tricky about timing, if you run it on really slow platform, it might fail."
+echo "NOTICE: Testing service $SERVICE"
 
 cleanup() {
     if [ $SMSD_PID -ne 0 ] ; then
@@ -17,11 +20,10 @@ trap cleanup INT QUIT EXIT
 
 cd @CMAKE_CURRENT_BINARY_DIR@
 
-rm -rf smsd-test
-mkdir smsd-test
-cd smsd-test
-# Create database
-@SQLITE_BIN@ smsd.db < @CMAKE_CURRENT_SOURCE_DIR@/../docs/sql/sqlite.sql
+rm -rf smsd-test-$SERVICE
+mkdir smsd-test-$SERVICE
+cd smsd-test-$SERVICE
+
 # Dummy backend storage
 mkdir gammu-dummy
 # Create config file
@@ -29,29 +31,82 @@ cat > .smsdrc <<EOT
 [gammu]
 model = dummy
 connection = none
-port = @CMAKE_CURRENT_BINARY_DIR@/smsd-test/gammu-dummy
+port = @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/gammu-dummy
 gammuloc = /dev/null
 loglevel = textall
 
 [smsd]
+commtimeout = 1
+debuglevel = 255
+logfile = stderr
+runonreceive = @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/received.sh
+EOT
+
+# Create database and add driver specific configuration
+case $SERVICE in
+    dbi-sqlite3)
+        @SQLITE_BIN@ smsd.db < @CMAKE_CURRENT_SOURCE_DIR@/../docs/sql/sqlite.sql
+        cat >> .smsdrc <<EOT
 service = dbi
 driver = sqlite3
 database = smsd.db
-commtimeout = 1
-dbdir = @CMAKE_CURRENT_BINARY_DIR@/smsd-test/
-debuglevel = 255
-logfile = stderr
-runonreceive = @CMAKE_CURRENT_BINARY_DIR@/smsd-test/received.sh
+dbdir = @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/
 EOT
+        ;;
+    dbi-pgsql)
+        echo "DROP TABLE IF EXISTS daemons, gammu, inbox, outbox, outbox_multipart, pbk, pbk_groups, phones, sentitems;" | PGPASSWORD=@PSQL_PASSWORD@ @PSQL_BIN@ -U @PSQL_USER@ @PSQL_DATABASE@
+        PGPASSWORD=@PSQL_PASSWORD@ @PSQL_BIN@ -U @PSQL_USER@ @PSQL_DATABASE@ < @CMAKE_CURRENT_SOURCE_DIR@/../docs/sql/pgsql.sql
+        cat >> .smsdrc <<EOT
+service = dbi
+driver = pgsql
+pc = localhost
+database = @PSQL_DATABASE@
+user = @PSQL_USER@
+password = @PSQL_PASSWORD@
+EOT
+        ;;
+    pgsql)
+        echo "DROP TABLE IF EXISTS daemons, gammu, inbox, outbox, outbox_multipart, pbk, pbk_groups, phones, sentitems;" | PGPASSWORD=@PSQL_PASSWORD@ @PSQL_BIN@ -U @PSQL_USER@ @PSQL_DATABASE@
+        PGPASSWORD=@PSQL_PASSWORD@ @PSQL_BIN@ -U @PSQL_USER@ @PSQL_DATABASE@ < @CMAKE_CURRENT_SOURCE_DIR@/../docs/sql/pgsql.sql
+        cat >> .smsdrc <<EOT
+service = pgsql
+pc = localhost
+database = @PSQL_DATABASE@
+user = @PSQL_USER@
+password = @PSQL_PASSWORD@
+EOT
+        ;;
+    dbi-mysql)
+        echo "DROP TABLE IF EXISTS  daemons, gammu, inbox, outbox, outbox_multipart, pbk, pbk_groups, phones, sentitems;" | @MYSQL_BIN@ -u@MYSQL_USER@ -p@MYSQL_PASSWORD@ @MYSQL_DATABASE@
+        @MYSQL_BIN@ -u@MYSQL_USER@ -p@MYSQL_PASSWORD@ @MYSQL_DATABASE@ < @CMAKE_CURRENT_SOURCE_DIR@/../docs/sql/mysql.sql
+        cat >> .smsdrc <<EOT
+service = dbi
+driver = mysql
+database = @MYSQL_DATABASE@
+user = @MYSQL_USER@
+password = @MYSQL_PASSWORD@
+EOT
+        ;;
+    mysql)
+        echo "DROP TABLE IF EXISTS  daemons, gammu, inbox, outbox, outbox_multipart, pbk, pbk_groups, phones, sentitems;" | @MYSQL_BIN@ -u@MYSQL_USER@ -p@MYSQL_PASSWORD@ @MYSQL_DATABASE@
+        @MYSQL_BIN@ -u@MYSQL_USER@ -p@MYSQL_PASSWORD@ @MYSQL_DATABASE@ < @CMAKE_CURRENT_SOURCE_DIR@/../docs/sql/mysql.sql
+        cat >> .smsdrc <<EOT
+service = mysql
+database = @MYSQL_DATABASE@
+user = @MYSQL_USER@
+password = @MYSQL_PASSWORD@
+EOT
+        ;;
+esac
 
-cat > @CMAKE_CURRENT_BINARY_DIR@/smsd-test/received.sh << EOT
+cat > @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/received.sh << EOT
 #!@SH_BIN@
-echo "\$@" >> @CMAKE_CURRENT_BINARY_DIR@/smsd-test/received.log
+echo "\$@" >> @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/received.log
 EOT
-chmod +x @CMAKE_CURRENT_BINARY_DIR@/smsd-test/received.sh
+chmod +x @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/received.sh
 
-CONFIG_PATH="@CMAKE_CURRENT_BINARY_DIR@/smsd-test/.smsdrc"
-DUMMY_PATH="@CMAKE_CURRENT_BINARY_DIR@/smsd-test/gammu-dummy"
+CONFIG_PATH="@CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/.smsdrc"
+DUMMY_PATH="@CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/gammu-dummy"
 
 mkdir -p $sms $DUMMY_PATH/sms/1
 mkdir -p $sms $DUMMY_PATH/sms/2
@@ -84,7 +139,7 @@ sleep 25
 
 @CMAKE_CURRENT_BINARY_DIR@/gammu-smsd-monitor -C -c "$CONFIG_PATH" -l 1 -d 0
 
-if [ `wc -l < @CMAKE_CURRENT_BINARY_DIR@/smsd-test/received.log` -ne 6 ] ; then
+if [ `wc -l < @CMAKE_CURRENT_BINARY_DIR@/smsd-test-$SERVICE/received.log` -ne 6 ] ; then
     echo "Wrong number of messages received!"
     exit 1
 fi
