f94b5a8437a3f6271d686dbaa0989c2a5e798e85
Michal Čihař <michal@cihar.com>
2009-05-22 09:37:08 +0000

    Fix redefinition of some symbols (bug #908).

--- libgammu/device/serial/ser_unx.c
+++ libgammu/device/serial/ser_unx.c
@@ -37,21 +37,15 @@
 #ifdef __NetBSD__
 #  define FNONBLOCK O_NONBLOCK
 
+#ifndef B57600
 #  define  B57600   0010001
+#endif
+#ifndef B115200
 #  define  B115200  0010002
+#endif
+#ifndef B230400
 #  define  B230400  0010003
-#  define  B460800  0010004
-#  define  B500000  0010005
-#  define  B576000  0010006
-#  define  B921600  0010007
-#  define  B1000000 0010010
-#  define  B1152000 0010011
-#  define  B1500000 0010012
-#  define  B2000000 0010013
-#  define  B2500000 0010014
-#  define  B3000000 0010015
-#  define  B3500000 0010016
-#  define  B4000000 0010017
+#endif
 #endif
 
 typedef struct {
