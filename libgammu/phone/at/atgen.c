9be13ccf70262717d3453eb6c433a8b48027afcf
Michal Čihař <michal@cihar.com>
2009-07-07 14:52:03 +0000

    Ignore also this one.

--- libgammu/phone/at/atgen.c
+++ libgammu/phone/at/atgen.c
@@ -4891,6 +4891,7 @@ GSM_Reply_Function ATGENReplyFunctions[] = {
 
 {ATGEN_GenericReplyIgnore, 	"^RSSI:"		,0x00,0x00,ID_IncomingFrame	 },
 {ATGEN_GenericReplyIgnore, 	"^BOOT:"		,0x00,0x00,ID_IncomingFrame	 },
+{ATGEN_GenericReplyIgnore, 	"^MODE:"		,0x00,0x00,ID_IncomingFrame	 },
 {ATGEN_GenericReplyIgnore, 	"^DSFLOWRPT:"		,0x00,0x00,ID_IncomingFrame	 },
 
 #ifdef GSM_ENABLE_ATOBEX
