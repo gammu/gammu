25447c2aa4b16f4748aaa3a6aef99768ff9aa4c2
Michal Čihař <michal@cihar.com>
2009-08-27 11:58:21 +0200

    Add test case for config file parsing.
    bug #987

---
+++ tests/config.c
@@ -0,0 +1,33 @@
+/**
+ * Config file parsing tests.
+ */
+
+
+#include <gammu.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "common.h"
+
+int main(int argc, char **argv)
+{
+    GSM_Error error;
+    GSM_Config cfg  = {"", "", NULL, NULL, NULL, NULL, NULL, NULL, FALSE, "", "", "", "", "", {0}};
+    INI_Section *ini = NULL;
+
+	/* Check parameters */
+	if (argc != 2) {
+		printf("Not enough parameters!\nUsage: config config_file\n");
+		return 1;
+	}
+
+	error = GSM_FindGammuRC(&ini, argv[1]);
+	gammu_test_result(error, "GSM_FindGammuRC");
+
+	error = GSM_ReadConfig(ini, &cfg, 0);
+	gammu_test_result(error, "GSM_ReadConfig");
+
+    printf("DEBUG_LEVEL: '%s'\n", cfg.DebugLevel);
+
+    return 0;
+}
