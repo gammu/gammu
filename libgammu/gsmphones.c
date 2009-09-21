68317f2a1674f9f297f7face77d6570c49a6c6f5
Michal Čihař <michal@cihar.com>
2009-09-21 11:28:41 +0200

    Add ID for SAMSUNG SGH-D980.

--- libgammu/gsmphones.c
+++ libgammu/gsmphones.c
@@ -409,6 +409,7 @@ GSM_PhoneModel allmodels[] = {
 	{"J750",	  "SGH-J750" ,	  "",				   {F_NO_ATOBEX, 0}},
 	{"J700",	  "SGH-J700" ,	  "",				   {F_NO_ATOBEX, 0}},
 	{"J700",	  "SAMSUNG SGH-J700" , "",			   {F_NO_ATOBEX, 0}},
+	{"D980",	  "SAMSUNG SGH-D980" , "",			   {F_NO_ATOBEX, 0}},
 
 	/* Ericsson/Sony Ericsson */
 	{"T28s",	"1101101-BVT28s","",				   {F_OBEX, 0}},
