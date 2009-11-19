0fc64ea4cf0c3158ce854c104ae26daead8e17b5
Michal Čihař <michal@cihar.com>
2009-11-19 15:18:25 +0100

    Setting version to 1.26.92

--- python/setup.py
+++ python/setup.py
@@ -40,7 +40,7 @@ import datetime
 import string
 
 # some defines
-VERSION = '1.26.91'
+VERSION = '1.26.92'
 VERSION_TUPLE = tuple(map(int, VERSION.split('.')))
 GAMMU_REQUIRED = VERSION_TUPLE
 PYTHON_REQUIRED = (2,3)
