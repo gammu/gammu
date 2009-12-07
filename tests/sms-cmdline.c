ea645377307a03d271dea17337f1a0987f181bc7
Michal Čihař <michal@cihar.com>
2009-12-07 14:23:52 +0100

    Flush logs before outputting message.

--- tests/sms-cmdline.c
+++ tests/sms-cmdline.c
@@ -21,6 +21,7 @@ int main(int argc, char **argv)
 
 	error = CreateMessage(&type, &sms, argc, 1, argv, NULL);
 	gammu_test_result(error, "CreateMessage");
+	fflush(stderr);
 
 	DisplayMultiSMSInfo(&sms, FALSE, TRUE, NULL, NULL);
 	DisplayMultiSMSInfo(&sms, TRUE, TRUE, NULL, NULL);
