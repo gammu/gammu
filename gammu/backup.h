e7c8d93f174b42309c4528838daeb1384541cf2b
Michal Čihař <michal@cihar.com>
2009-12-09 13:22:58 +0100

    Include declaration in header.

--- gammu/backup.h
+++ gammu/backup.h
@@ -5,3 +5,4 @@ extern void AddNew(int argc, char *argv[]);
 extern void Restore(int argc, char *argv[]);
 extern void DoBackup(int argc, char *argv[]);
 extern void SaveFile(int argc, char *argv[]);
+extern GSM_Error ReadPhonebook(GSM_MemoryEntry **Phonebook, GSM_MemoryType MemoryType, const char *question, int max_entries);
