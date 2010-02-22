d79b84d843097dc1525d24bb71315207fa2356b1
Michal Čihař <michal@cihar.com>
2010-02-22 14:24:40 +0100

    No need to import sys here.

--- python/gammu/Data.py
+++ python/gammu/Data.py
@@ -30,8 +30,6 @@ this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
-import sys
-
 Connections = [
     'at',
     'at19200',
