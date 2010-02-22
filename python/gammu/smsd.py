d5e02ffc7e5d6ffc5e6562333e7c4197e51e3619
Michal Čihař <michal@cihar.com>
2010-02-22 15:02:55 +0100

    Define list of public classes.

--- python/gammu/smsd.py
+++ python/gammu/smsd.py
@@ -22,4 +22,6 @@ this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
+__all__ = ['SMSD']
+
 from gammu._gammu import SMSD
