33a7062cbcc8e78b01b14d208837b00866f9e662
Michal Čihař <michal@cihar.com>
2010-02-22 15:50:46 +0100

    Insert path at begining, overriding system wide modules.

--- python/doc/conf.py
+++ python/doc/conf.py
@@ -19,8 +19,7 @@ import sys, os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
-sys.path.append('@CMAKE_CURRENT_BINARY_DIR@')
+sys.path.insert(0, '@CMAKE_CURRENT_BINARY_DIR@')
 
 # General configuration
 # ---------------------
