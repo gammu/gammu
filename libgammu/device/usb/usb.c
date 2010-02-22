5e420ca43f192cef6ad9fa2a40506c0b95fa26aa
Michal Čihař <michal@cihar.com>
2010-02-22 13:33:53 +0100

    Better fix for compiling USB.

--- libgammu/device/usb/usb.c
+++ libgammu/device/usb/usb.c
@@ -8,13 +8,15 @@
  * Licensed under GNU GPL version 2 or later
  */
 
+#include <gammu-config.h>
+
+#include <libusb.h>
+#include "usb.h"
+
 #include "../../gsmstate.h"
 #include "../../gsmcomon.h"
 
 #if defined(GSM_ENABLE_DKU2PHONET) && defined(GSM_ENABLE_USBDEVICE)
-
-#include <libusb.h>
-#include "usb.h"
 
 /**
  * Nokia USB vendor ID.
