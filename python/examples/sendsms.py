8b601d9b8f92496789d01cee06aa1d8381aa669a
Michal Čihař <michal@cihar.com>
2010-03-29 14:21:45 +0200

    Document example.

--- python/examples/sendsms.py
+++ python/examples/sendsms.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
-# sample script to show how to send SMS
+# Sample script to show how to send SMS
 
 import gammu
-
-sm = gammu.StateMachine()
-sm.ReadConfig()
-sm.Init()
-
 import sys
+
+# Check parameters
 if len(sys.argv) != 2:
     print 'This requires one parameter containing number!'
     sys.exit(1)
 
-message = {'Text': 'python-gammu testing message', 'SMSC': {'Location': 1}, 'Number': sys.argv[1]}
+# Create object for talking with phone
+sm = gammu.StateMachine()
 
+# Read the configuration (~/.gammurc)
+sm.ReadConfig()
+
+# Connect to the phone
+sm.Init()
+
+# Prepare message data
+# We tell that we want to use first SMSC number stored in phone
+message = {
+    'Text': 'python-gammu testing message',
+    'SMSC': {'Location': 1},
+    'Number': sys.argv[1],
+}
+
+# Actually send the message
 sm.SendSMS(message)
