acff1f1949e233e9598b9866b23d26f130f85b09
Michal Čihař <michal@cihar.com>
2010-03-29 14:29:02 +0200

    Document examples.

--- python/examples/dialvoice.py
+++ python/examples/dialvoice.py
@@ -3,16 +3,23 @@
 import gammu
 import sys
 
+# Create object for talking with phone
 sm = gammu.StateMachine()
+
+# Read the configuration (~/.gammurc or from command line)
 if len(sys.argv) >= 2:
     sm.ReadConfig(Filename = sys.argv[1])
     del sys.argv[1]
 else:
     sm.ReadConfig()
+
+# Connect to the phone
 sm.Init()
 
+# Check whether we have a number to dial
 if len(sys.argv) != 2:
     print 'Usage: dialvoice.py NUMBER'
     sys.exit(1)
 
+# Dial a number
 sm.DialVoice(sys.argv[1])
--- python/examples/getallcalendar.py
+++ python/examples/getallcalendar.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
+# Example for reading calendar from phone
 
 import gammu
 
+# Create object for talking with phone
 sm = gammu.StateMachine()
+
+# Read the configuration (~/.gammurc)
 sm.ReadConfig()
+
+# Connect to the phone
 sm.Init()
 
+# Get number of calendar entries
 status = sm.GetCalendarStatus()
 
 remain = status['Used']
@@ -13,6 +20,7 @@ remain = status['Used']
 start = True
 
 while remain > 0:
+    # Read the entry
     if start:
         entry = sm.GetNextCalendar(Start = True)
         start = False
@@ -20,6 +28,7 @@ while remain > 0:
         entry = sm.GetNextCalendar(Location = entry['Location'])
     remain = remain - 1
 
+    # Display it
     print
     print '%-20s: %d' % ('Location',entry['Location'])
     print '%-20s: %s' % ('Type',entry['Type'])
