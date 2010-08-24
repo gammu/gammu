92278474cc578332b040e4355c8bcb6860213826
Michal Čihař <mcihar@novell.com>
2010-08-24 10:02:04 +0200

    Add testcase for backup crash (bug #1267).

---
+++ tests/read-backup.c
@@ -0,0 +1,35 @@
+#include <gammu.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "common.h"
+
+GSM_StateMachine *s;
+
+int main(int argc, char **argv)
+{
+	GSM_Error error;
+	GSM_Backup backup;
+	GSM_Debug_Info *debug_info;
+
+    if (argc != 2) {
+        printf("Usage: read-backup backup\n");
+        exit(1);
+    }
+
+	/* Configure debugging */
+	debug_info = GSM_GetGlobalDebug();
+	GSM_SetDebugFileDescriptor(stderr, FALSE, debug_info);
+	GSM_SetDebugLevel("textall", debug_info);
+
+	error = GSM_ReadBackupFile(argv[2], &backup, GSM_GuessBackupFormat(argv[2], FALSE));
+	gammu_test_result(error, "Read backup");
+
+    GSM_FreeBackup(&backup);
+
+	return 0;
+}
+
+/* Editor configuration
+ * vim: noexpandtab sw=8 ts=8 sts=8 tw=72:
+ */
+
