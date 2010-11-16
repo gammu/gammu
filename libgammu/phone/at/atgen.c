00f784bafa6b9551d32d8f3228b9f52b2b0efb83
Michal Čihař <mcihar@novell.com>
2010-11-16 13:34:09 +0100

    Revert "Try another way of detecting mobex support."
    This reverts commit b81576547ed104aee723028505066f2452bdba5a.

--- libgammu/phone/at/atgen.c
+++ libgammu/phone/at/atgen.c
@@ -2046,7 +2046,7 @@ GSM_Error ATGEN_Initialise(GSM_StateMachine *s)
 	if (!GSM_IsPhoneFeatureAvailable(s->Phone.Data.ModelInfo, F_NO_ATSYNCML)) {
 		smprintf(s, "Checking for SYNCML/OBEX support\n");
 		/* We don't care about error here */
-		ATGEN_WaitForAutoLen(s, "AT+SYNCML?\r", 0x00, 20, ID_SetOBEX);
+		ATGEN_WaitForAutoLen(s, "AT+SYNCML=?\r", 0x00, 20, ID_SetOBEX);
 		error = ERR_NONE;
 	}
 
@@ -5390,7 +5390,7 @@ GSM_Reply_Function ATGENReplyFunctions[] = {
 {ATGEN_GenericReply,		"AT+CKPD="		,0x00,0x00,ID_PressKey		 },
 {ATGEN_ReplyGetSIMIMSI,		"AT+CIMI" 	 	,0x00,0x00,ID_GetSIMIMSI	 },
 {ATGEN_ReplyCheckProt,		"AT+CPROT=?" 	 	,0x00,0x00,ID_SetOBEX		 },
-{ATGEN_ReplyCheckSyncML,	"AT+SYNCML?" 	 	,0x00,0x00,ID_SetOBEX		 },
+{ATGEN_ReplyCheckSyncML,	"AT+SYNCML=?" 	 	,0x00,0x00,ID_SetOBEX		 },
 {ATGEN_GenericReply,		"AT+XLNK=?" 	 	,0x00,0x00,ID_SetOBEX		 },
 
 {ATGEN_ReplyGetCNMIMode,	"AT+CNMI=?"		,0x00,0x00,ID_GetCNMIMode	 },
