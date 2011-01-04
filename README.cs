3fcacf321a811ef2cab983b5188cd90222583d9f
Michal Čihař <michal@cihar.com>
2011-01-04 07:49:57 +0100

    Update Czech translation.

---
+++ README.cs
@@ -0,0 +1,31 @@
+Gammu All Mobile Management Utilities
+=====================================
+
+Gammu je knihovna a utilita pro příkazovou řádku pro práci s mobilními
+telefont. Je vydáno pod GNU GPL verze 2.
+
+Projekt byl původně veden Marcinem Wiackem. Byl založen na zdrojových kódech
+projektů Gnokii <http://www.gnokii.org> a pozdějí MyGnokii
+<http://www.mwiacek.com>. Gammu bylo (až do verze 0.58) nazýváno MyGnokii2.
+
+V současné době projekt spravuje Michal Čihař <michal@cihar.com> s pomocí
+mnoha přispěvovatelů.
+
+
+Více informací
+================
+
+Více informací naleznete na <http://cs.wammu.eu/gammu/>.
+
+There is also Gammu manual available in docs/manual. You can build HTML
+version of it using make manual-html which is viewable online at
+<http://wammu.eu/docs/manual/>.
+
+
+Zpětná vazba a hlášení chyb
+================================
+
+Any feedback is welcome, see <http://wammu.eu/support/> for information how
+to contact developers.
+
+# vim: et ts=4 sw=4 sts=4 tw=72 spell spelllang=en_us
