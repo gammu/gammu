4ab7023f615686f61d804bd5a90f5bb68b1b4fdc
Michal Čihař <mcihar@novell.com>
2011-01-28 14:52:09 +0100

    Improve searching for glib/gobject.

--- cmake/FindGobject.cmake
+++ cmake/FindGobject.cmake
@@ -1,12 +1,32 @@
-pkg_check_modules(GOBJECT gobject-2.0)
+pkg_check_modules(GOBJECT_PKG gobject-2.0)
 
-# Find Gobject even if pkg-config is not working (eg. cross compiling to Windows)
-if (NOT GOBJECT_FOUND)
+if (GOBJECT_PKG_FOUND)
+    find_path(GOBJECT_INCLUDE_DIR  NAMES glib-object.h PATH_SUFFIXES glib-2.0
+       PATHS
+       ${GOBJECT_PKG_INCLUDE_DIRS}
+       /usr/include/glib-2.0
+       /usr/include
+       /usr/local/include
+    )
+    find_library(GOBJECT_LIBRARIES NAMES gobject-2.0
+       PATHS
+       ${GOBJECT_PKG_LIBRARY_DIRS}
+       /usr/lib
+       /usr/local/lib
+    )
+else (GOBJECT_PKG_FOUND)
+    # Find Gobject even if pkg-config is not working (eg. cross compiling to Windows)
     find_path(GOBJECT_INCLUDE_DIRS NAMES glib-object.h PATH_SUFFIXES glib-2.0)
 
     find_library(GOBJECT_LIBRARIES NAMES gobject-2.0)
-    if (GOBJECT_INCLUDE_DIRS AND GOBJECT_LIBRARIES)
-        set(GOBJECT_FOUND TRUE)
-    endif (GOBJECT_INCLUDE_DIRS AND GOBJECT_LIBRARIES)
-endif (NOT GOBJECT_FOUND)
+endif (GOBJECT_PKG_FOUND)
 
+if(GOBJECT_INCLUDE_DIR AND GOBJECT_LIBRARIES)
+   set(GOBJECT_FOUND TRUE CACHE INTERNAL "gobject-2.0 found")
+   message(STATUS "Found gobject-2.0: ${GOBJECT_INCLUDE_DIR}, ${GOBJECT_LIBRARIES}")
+else(GOBJECT_INCLUDE_DIR AND GOBJECT_LIBRARIES)
+   set(GOBJECT_FOUND FALSE CACHE INTERNAL "gobject-2.0 found")
+   message(STATUS "gobject-2.0 not found.")
+endif(GOBJECT_INCLUDE_DIR AND GOBJECT_LIBRARIES)
+
+mark_as_advanced(GOBJECT_INCLUDE_DIR GOBJECT_LIBRARIES)
