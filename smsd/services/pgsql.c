85d2632ea20604283f68fb1992bf8ed811744626
Michal Čihař <michal@cihar.com>
2011-03-17 07:35:42 +0100

    Adjust escaping

--- smsd/services/pgsql.c
+++ smsd/services/pgsql.c
@@ -172,7 +172,7 @@ unsigned long long SMSDPgSQL_SeqID(GSM_SMSDConfig * Config, const char *seq_id)
 	PGresult *rc;
 	int Status;
 
-	snprintf(buff, sizeof(buff), "SELECT currval('%s')", seq_id);
+	snprintf(buff, sizeof(buff), "SELECT currval(\"%s\")", seq_id);
 	rc = PQexec(Config->conn.pg, buff);
 	if ((rc == NULL) || ((Status = PQresultStatus(rc)) != PGRES_COMMAND_OK && (Status != PGRES_TUPLES_OK))) {
 		return 0;
