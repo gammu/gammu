90936b038685c01d880a47047d334ac2090d8b6a
Michal Čihař <mcihar@novell.com>
2011-04-18 11:23:29 +0200

    Set windres

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -5,6 +5,7 @@ SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 SET(CMAKE_AR i686-w64-mingw32-ar)
 SET(CMAKE_LD i686-w64-mingw32-ld)
