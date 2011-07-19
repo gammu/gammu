e8bb19132b211d4891d7d182957e73ff167b007e
Michal Čihař <mcihar@suse.cz>
2011-07-19 11:02:06 +0200

    Fixed usage of dbi plugins from Python module.
    The libdbi drivers use symbols from libdbi, which is by default not
    accessible as the Python module is loaded with RTLD_LOCAL. By forcing
    RTDL_GLOBAL we override this, hopefully without breaking something else.

--- python/gammu/__init__.py
+++ python/gammu/__init__.py
@@ -30,8 +30,43 @@ __all__ = [
     'exception',
     ]
 
-# Import core library
-from gammu._gammu import *
+# Import core library, we need to load it into global namespace
+# as otherwise libdbi plugins fail to find some symbols
+import sys
+if not hasattr(sys,'getdlopenflags'):
+    from gammu._gammu import *
+else:
+    try:
+        from dl import RTLD_GLOBAL, RTLD_NOW
+    except ImportError:
+        RTLD_GLOBAL = -1
+        RTLD_NOW = -1
+        try:
+            import os
+            osname = os.uname()[0]
+            if osname == 'Linux' or osname == 'SunOS':
+                RTLD_GLOBAL = 0x00100
+                RTLD_NOW = 0x00002
+            elif osname == 'Darwin':
+                RTLD_GLOBAL = 0x8
+                RTLD_NOW = 0x2
+        except:
+             pass
+
+    if RTLD_GLOBAL != -1 and RTLD_NOW != -1:
+        try:
+            flags = sys.getdlopenflags()
+            sys.setdlopenflags(RTLD_GLOBAL | RTLD_NOW)
+            try:
+                from gammu._gammu import *
+            finally:
+                sys.setdlopenflags(flags)
+        except:
+            from gammu._gammu import *
+    else:
+        from gammu._gammu import *
+    del RTLD_GLOBAL
+    del RTLD_NOW
 
 __version__ = 'Gammu %s, python-gammu %s' % (
         Version()[0],
