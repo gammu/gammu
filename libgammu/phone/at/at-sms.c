422f6f90e4c35db8f808dec7de012f79e918b5ff
Michal Čihař <mcihar@suse.cz>
2011-12-07 16:27:34 +0100

    Check for real length

--- libgammu/phone/at/at-sms.c
+++ libgammu/phone/at/at-sms.c
@@ -866,8 +866,11 @@ GSM_Error ATGEN_ReplyGetSMSMessage(GSM_Protocol_Message *msg, GSM_StateMachine *
 					/* If UDH available, treat it as Unicode or 8 bit */
 					if ((firstbyte & 0x40)!=0x40) {
 						sms->UDH.Type	= UDH_NoUDH;
-						sms->Length	= TPUDL;
-						EncodeUnicode(sms->Text, msg->Buffer+Priv->Lines.numbers[2*2], TPUDL);
+						sms->Length	= strlen(msg->Buffer+Priv->Lines.numbers[2*2]);
+						if (TPUDL != sms->Length) {
+							smprintf(s, "WARNING: Indicated message length (%d) does not match real (%d)\n", TPUDL, sms->Length);
+						}
+						EncodeUnicode(sms->Text, msg->Buffer+Priv->Lines.numbers[2*2], sms->Length);
 						break;
 					}
 				case SMS_Coding_Unicode_No_Compression:
