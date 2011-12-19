80faac54b67832d9516665c4a499a34bfa471e50
Michal Čihař <mcihar@suse.cz>
2011-12-19 08:44:01 +0100

    Add Polish docs

---
+++ README.pl
@@ -0,0 +1,35 @@
+Gammu Zarządzanie wszystkimi telefonami
+========================================
+
+Gammu jest biblioteką i linią komend narzędzia dla telefonów
+komórkowych. Jest udostępniany na licencji GNU GPL w wersji 2.
+
+Został on zainicjowany przez Marcina Wiącka i innych ludzi. Oryginalny kod
+był oparty na Gnokii <http://www.gnokii.org> i później mygnokii
+<http://www.mwiacek.com>. Gammu był (do wersji 0.58) z nazwą MyGnokii2.
+
+Obecnie projekt jest prowadzony przez Michal Cihar <michal@cihar.com> z
+pomocą wielu użytkowników.
+
+
+Więcej informacji
+==================
+
+Możesz znaleźć więcej informacji na temat <http://wammu.eu/gammu/>.
+
+Istnieje również podręcznik Gammu dostępny w docs/manual. Możesz budować
+wersję HTML za pomocą zrobić manual-html, który jest na:
+<http://wammu.eu/docs/manual/>.
+
+
+Opinie i raporty o błędach
+============================
+
+Wszelkie uwagi są mile widziane, patrz <http://wammu.eu/support/> po
+informację jak skontaktować się z programistami.
+
+
+Wsparcie programistów
+======================
+
+Można docenić pracę programistów na <http://wammu.eu/donate/>.
