ebee287372815e53f76f7548c759e8c47594d1a1
Michal Čihař <mcihar@suse.cz>
2013-02-21 10:26:08 +0100

    Provide context for Call event type as it collides with calling somebody

--- helper/locales.h
+++ helper/locales.h
@@ -17,11 +17,16 @@
 #ifdef LIBINTL_LIB_FOUND
 #include <libintl.h>
 #define _(x) gettext(x)
+#ifndef LIBINTL_HAS_PGETTEXT
+#define pgettext(context, string) gettext(context "\004" string)
+#endif
 #else
 #define _(x) (x)
 #define gettext(x) (x)
+#define pgettext(context, string) (string)
 #define ngettext(singular, plural, number) (number == 1 ? singular : plural)
 #endif
+
 
 #define N_(x) x
 
