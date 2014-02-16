b1cfeb80e9eeb18ae5301fd53c18b2a7befcd717
Michal Čihař <michal@cihar.com>
2014-02-16 12:23:14 +0100

    Update po files

--- INSTALL.es
+++ INSTALL.es
@@ -1,5 +1,5 @@
 Gammu All Mobile Management Utilities - Instalación
-====================================================
+===================================================
 
 Ver docs/manual/project/install.rst para la guía de instalación o el
 capítulo "Compilando Gammu" en el Manual de Gammu
--- INSTALL.pl
+++ INSTALL.pl
@@ -1,5 +1,5 @@
 Gammu Zarządzanie wszystkimi telefonami - Instalacja
-=====================================================
+====================================================
 
 Zobacz docs/manual/project/install.rst dla instrukcji instalacji lub w
 rozdziale "Kompilacja Gammu" w Podręczniku Gammu.
