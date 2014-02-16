f68590f92c3bb92c5a374bab0acd012752719fdd
Michal Čihař <michal@cihar.com>
2014-02-16 12:29:31 +0100

    Translate badge links

---
+++ README.cs
@@ -0,0 +1,46 @@
+Gammu All Mobile Management Utilities
+=====================================
+
+Gammu je knihovna a utilita pro příkazovou řádku pro práci s mobilními
+telefont. Je vydáno pod GNU GPL verze 2.
+
+Projekt byl původně veden Marcinem Wiackem. Byl založen na zdrojových kódech
+projektů Gnokii <http://www.gnokii.org> a pozdějí MyGnokii
+<http://www.mwiacek.com>. Gammu bylo (až do verze 0.58) nazýváno MyGnokii2.
+
+V současné době projekt spravuje Michal Čihař <michal@cihar.com> s pomocí
+mnoha přispěvovatelů.
+
+.. image:: https://travis-ci.org/gammu/gammu.png?branch=master
+    :target: https://travis-ci.org/gammu/gammu
+
+.. image:: https://l10n.cihar.com/widgets/gammu-status-badge.png
+    :alt: Translation status :target:
+    https://l10n.cihar.com/engage/gammu/?utm_source=widget
+
+.. image:: https://d2weczhvl823v0.cloudfront.net/gammu/gammu/trend.png
+   :alt: Bitdeli badge :target: https://bitdeli.com/free
+
+
+Více informací
+==============
+
+Více informací naleznete na <http://cs.wammu.eu/gammu/>.
+
+Další dokumentaci naleznete v Manuálu Gammu, který je v adresáři
+docs/manual. Můžete si vytvořit jeho HTML verzi pro prohlížení pomocí make
+manual-html, nebo si ji prohlédnout online na
+<http://wammu.eu/docs/manual/>.
+
+
+Zpětná vazba a hlášení chyb
+===========================
+
+Jakákoliv zpětná vazba je vítána, informace jak kontaktovat vývojáře
+naleznete na <http://cs.wammu.eu/support/>.
+
+
+Podpořte vývojáře
+=================
+
+Práci vývojářů můžete ocenit na <http://cs.wammu.eu/donate/>.
---
+++ README.pl
@@ -0,0 +1,45 @@
+Gammu Zarządzanie wszystkimi telefonami
+=======================================
+
+Gammu jest biblioteką i linią komend narzędzia dla telefonów
+komórkowych. Jest udostępniany na licencji GNU GPL w wersji 2.
+
+Został on zainicjowany przez Marcina Wiącka i innych ludzi. Oryginalny kod
+był oparty na Gnokii <http://www.gnokii.org> i później mygnokii
+<http://www.mwiacek.com>. Gammu był (do wersji 0.58) z nazwą MyGnokii2.
+
+Obecnie projekt jest prowadzony przez Michal Cihar <michal@cihar.com> z
+pomocą wielu użytkowników.
+
+.. image:: https://travis-ci.org/gammu/gammu.png?branch=master
+    :target: https://travis-ci.org/gammu/gammu
+
+.. image:: https://l10n.cihar.com/widgets/gammu-status-badge.png
+    :alt: Translation status :target:
+    https://l10n.cihar.com/engage/gammu/?utm_source=widget
+
+.. image:: https://d2weczhvl823v0.cloudfront.net/gammu/gammu/trend.png
+   :alt: Bitdeli badge :target: https://bitdeli.com/free
+
+
+Więcej informacji
+=================
+
+Możesz znaleźć więcej informacji na temat <http://wammu.eu/gammu/>.
+
+Istnieje również podręcznik Gammu dostępny w docs/manual. Możesz budować
+wersję HTML za pomocą zrobić manual-html, który jest na:
+<http://wammu.eu/docs/manual/>.
+
+
+Opinie i raporty o błędach
+==========================
+
+Wszelkie uwagi są mile widziane, patrz <http://wammu.eu/support/> po
+informację jak skontaktować się z programistami.
+
+
+Wsparcie programistów
+=====================
+
+Można docenić pracę programistów na <http://wammu.eu/donate/>.
