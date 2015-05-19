gammu/gammu

commit 9ec31586756a96003a3f5bd8f6d21d6eb3c6e170

Check error code

gammu/depend/siemens/dsiemens.c
@@ -243,12 +243,14 @@ GSM_Error ATSIEMENS_GetSAT(GSM_StateMachine *sm)
 
         strcpy(req, "AT^SSTK=?\r");
         error = GSM_WaitForAutoLen(sm, req, 0x00, 3, ID_User1);
+	Print_Error(error);
 
     	for (i=0;i<3;i++){
 		len				= strlen(reqSAT[i]);
 		sm->Protocol.Data.AT.EditMode 	= TRUE;
         	sprintf(req, "AT^SSTK=%i,1\r",len/2);
         	error = GSM_WaitForAutoLen(sm, req, 0x00, 3, ID_User1);
+		Print_Error(error);
 		sm->Phone.Data.DispatchError	= ERR_TIMEOUT;
 		sm->Phone.Data.RequestID		= ID_User1;
     		error = sm->Protocol.Functions->WriteMessage(sm, reqSAT[i], len, 0x00);

commit 0babb16130d8f3c8dd4f38ffe79451b2395e8842

Fix siemenssatnetmon
The siemenssatnetmon command has been broken since 9ec31586756a96,
which adds checks for errors, as ATSIEMENS_Reply_GetSAT always
returns an error when called in AT_Reply_SMSEdit state. Fix that.

Signed-off-by: Michal Čihař <michal@cihar.com>

gammu/depend/siemens/dsiemens.c
@@ -37,8 +37,14 @@ GSM_Error ATSIEMENS_Reply_GetSAT(GSM_Protocol_Message *msg, GSM_StateMachine *sm
     	int				freq_tmp,frequency[24];
    	GSM_NetworkInfo			Network;
 
-    	if (Priv->ReplyState!=AT_Reply_OK) return ERR_UNKNOWN;
-    	if (sm->Protocol.Data.AT.EditMode) sm->Protocol.Data.AT.EditMode = FALSE;
+	if (Priv->ReplyState != AT_Reply_OK) {
+		return ERR_UNKNOWN;
+	}
+	if (sm->Protocol.Data.AT.EditMode) {
+		sm->Protocol.Data.AT.EditMode = FALSE;
+		return ERR_NONE;
+	}
+
 	if (strstr(GetLineString(msg->Buffer,&Priv->Lines,2),"SSTK")) {
     	    length = strlen(GetLineString(msg->Buffer,&Priv->Lines,2))-7;
     	    DecodeHexBin(buf, GetLineString(msg->Buffer,&Priv->Lines,2)+7,length);