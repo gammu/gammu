0bf122c1772c99767ca2fadb0a00e2b7aa5f8015
Michal Čihař <michal@cihar.com>
2015-06-04 09:02:58 +0200

    Link documentation with python
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -53,7 +53,9 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['breathe', 'configext', 'sphinx.ext.graphviz']
+extensions = ['breathe', 'configext', 'sphinx.ext.graphviz', 'sphinx.ext.intersphinx']
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['@CMAKE_CURRENT_SOURCE_DIR@/.templates']
