5ce76a298ab2424957db3c071cb8494b22dad8f8
Michal Čihař <michal@cihar.com>
2015-06-04 09:13:33 +0200

    Remove not used static dir
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -167,7 +167,7 @@ html_theme_path = ['.']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['@CMAKE_CURRENT_SOURCE_DIR@/doc/.static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
