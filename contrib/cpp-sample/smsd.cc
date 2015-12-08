410da1efb1eec787560330ac2620892970351162
Michal Čihař <michal@cihar.com>
2015-12-08 11:18:00 +0100

    Actuall add c++ example
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ contrib/cpp-sample/smsd.cc
@@ -0,0 +1,12 @@
+#include <gammu-smsd.h>
+
+int main(int rgc, char** argv)
+{
+    GSM_SMSDConfig *smsd;
+    
+    smsd = SMSD_NewConfig("c++smsd");
+    
+    SMSD_FreeConfig(smsd);
+
+    return 0;
+}
