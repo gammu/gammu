c814d56bb46050691841360b17a0e648c473b02b
Michal Čihař <michal@cihar.com>
2016-08-21 19:29:32 +0200

    Check library can be used when finding
    This will hopefully avoid problems on Windows where we find 32-bit
    libraries on 64-bit build.
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ cmake/MacroCheckLibraryWorks.cmake
@@ -0,0 +1,36 @@
+MACRO (CHECK_LIBRARY_WORKS _header _include _library _target)
+
+    if (DEFINED ${_target})
+        message(STATUS "${_target} passed")
+    else()
+
+    file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.c"
+      "
+//TEST
+#include <${_header}>
+
+int main(void) {
+    return 0;
+}
+")
+
+    try_compile(TMP_${_target}
+      ${CMAKE_BINARY_DIR}
+      ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.c
+      LINK_LIBRARIES ${_library}
+      COMPILE_DEFINITIONS -I${_include}
+      OUTPUT_VARIABLE OUTPUT
+    )
+    
+    if (TMP_${_target})
+        message(STATUS "${_target} passed")
+        set(${_target} TRUE CACHE INTERNAL "Test ${_target}")
+    else()
+        message(STATUS "${_target} failed")
+        set(${_target} FALSE CACHE INTERNAL "Test ${_target}")
+    endif()
+
+    
+    endif()
+
+ENDMACRO()
