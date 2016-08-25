9b91e6988de26bc4c8fc4132d11e8abe81ad153d
Michal Čihař <michal@cihar.com>
2016-08-25 15:23:26 +0200

    Download silently
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -6,7 +6,7 @@ if [ ! -f cobertura1.xml ] ; then
     exit 0
 fi
 
-curl -o codecov.sh https://codecov.io/bash
+curl -s -o codecov.sh https://codecov.io/bash
 
 # Upload in chunks of 15 files
 ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 30 bash ./codecov.sh -X nocolor -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
