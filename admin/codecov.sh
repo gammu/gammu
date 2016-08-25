b88f0e3472cc78cda2834cec66bcde426f421a70
Michal Čihař <michal@cihar.com>
2016-08-25 09:56:19 +0200

    Use find for files discovery to avoid problems with too long commandline
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -7,4 +7,4 @@ if [ ! -d coverage ] ; then
 fi
 
 #echo ./coverage/*/cobertura.xml | sed -e 's/^/-f /' | xargs codecov -X search -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
-echo ./coverage/*/cobertura.xml | sed -e 's/^/-f /' | xargs bash -x ./admin/codecov-uploader.sh -X nocolor -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
+find ./coverage -name cobertura.xml | sed -e 's/^/-f /' | xargs bash -x ./admin/codecov-uploader.sh -X nocolor -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
