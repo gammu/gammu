d6a802afe8f0bd0a56e70a24c72e20b5739509d0
Michal Čihař <michal@cihar.com>
2016-08-25 09:23:31 +0200

    Try codecov upload using bash script
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -6,4 +6,5 @@ if [ ! -d coverage ] ; then
     exit 0
 fi
 
-echo ./coverage/*/cobertura.xml | sed -e 's/^/-f /' | xargs codecov -X search -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
+#echo ./coverage/*/cobertura.xml | sed -e 's/^/-f /' | xargs codecov -X search -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
+echo ./coverage/*/cobertura.xml | sed -e 's/^/-f /' | xargs bash -x ./admin/codecov-uploader.sh -X nocolor -X gcov -F "${CODECOV_FLAG},${CONFIGURATION}"
