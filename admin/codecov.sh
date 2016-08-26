ae34c31ae623c815e59f451babe619c078054743
Michal Čihař <michal@cihar.com>
2016-08-26 13:55:11 +0200

    Remove download of codecov script, we embedd one right now
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -9,8 +9,8 @@ fi
 export PATH="C:/MinGW/bin:$PATH"
 
 # Download codecov script
-curl -s -o codecov.sh https://codecov.io/bash
+#curl -s -o codecov.sh https://codecov.io/bash
 
 # Upload in chunks of 25 files
 #ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 10 bash ./codecov.sh -X nocolor -X gcov
-ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 50 ./admin/codecov-uploader.sh -X nocolor -X gcov
+ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 20 ./admin/codecov-uploader.sh -X nocolor -X gcov
