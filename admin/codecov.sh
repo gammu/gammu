67fc3936e5c7ed30480a7f4a8d7c290f61030eac
Michal Čihař <michal@cihar.com>
2016-08-26 11:33:35 +0200

    Add missing xargs
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -11,4 +11,4 @@ curl -s -o codecov.sh https://codecov.io/bash
 
 # Upload in chunks of 5 files
 #ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 10 bash ./codecov.sh -X nocolor -X gcov
-ls cobertura*.xml | sed -e 's/^/-f /' | ./admin/codecov-uploader.sh -X nocolor -X gcov
+ls cobertura*.xml | sed -e 's/^/-f /' | xargs ./admin/codecov-uploader.sh -X nocolor -X gcov
