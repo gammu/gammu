94024dbe4d18e87e8766de7671866bb1b9261a59
Michal Čihař <michal@cihar.com>
2016-08-26 12:21:35 +0200

    Use bigger chunks
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -9,6 +9,6 @@ fi
 # Download codecov script
 curl -s -o codecov.sh https://codecov.io/bash
 
-# Upload in chunks of 5 files
+# Upload in chunks of 50 files
 #ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 10 bash ./codecov.sh -X nocolor -X gcov
-ls cobertura*.xml | sed -e 's/^/-f /' | xargs ./admin/codecov-uploader.sh -X nocolor -X gcov
+ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 100 ./admin/codecov-uploader.sh -X nocolor -X gcov
