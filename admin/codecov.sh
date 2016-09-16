977ef4ea423bcd1b6bd7442935e249d17a14615c
Michal Čihař <michal@cihar.com>
2016-09-16 20:44:05 +0200

    Pass test platform as flag to codecov
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- admin/codecov.sh
+++ admin/codecov.sh
@@ -12,4 +12,4 @@ export PATH="C:/MinGW/bin:$PATH"
 curl -s -o codecov.sh https://codecov.io/bash
 
 # Upload in chunks of 5 files
-ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 10 bash ./codecov.sh -X nocolor -X gcov
+ls cobertura*.xml | sed -e 's/^/-f /' | xargs -n 10 bash ./codecov.sh -X nocolor -X gcov -F windows
