8d2c264e8bc161e502724bb669e17ce117312731
Michal Čihař <michal@cihar.com>
2017-03-30 09:16:59 +0200

    Adjust Cobertura XML merger to generate valid Cobertura XML
    We now follow http://cobertura.sourceforge.net/xml/coverage-04.dtd
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- contrib/coveragehelper/merge-cobertura.py
+++ contrib/coveragehelper/merge-cobertura.py
@@ -45,11 +45,13 @@ def get_line_rates(data):
         lines = len(data[item])
         hits = sum([1 for x in data[item].values() if x])
         result[item] = 1.0 * hits / lines
-        log(' * {0} = {1}'.format(item, result[item]))
+        log(' * {0} = {1} ({2} / {3})'.format(item, result[item], hits, lines))
         total_lines += lines
         total_hits += hits
 
     result['_'] = 1.0 * total_hits / total_lines
+    result['_hits'] = total_hits
+    result['_lines'] = total_lines
 
     return result
 
@@ -59,10 +61,25 @@ def write_data(data, handle):
 
     root = Element('coverage')
     root.set('line-rate', str(line_rates['_']))
+    root.set('branch-rate', '0')
+    root.set('complexity', '0')
+    root.set('branches-covered', '0')
+    root.set('branches-valid', '0')
+    root.set('timestamp', '0')
+    root.set('lines-covered', str(line_rates['_hits']))
+    root.set('lines-valid', str(line_rates['_lines']))
+    root.set('version', '0')
+
+    sources = SubElement(root, 'sources')
+    source = SubElement(sources, 'source')
+    source.text = 'c:'
+
     packages = SubElement(root, 'packages')
     package = SubElement(packages, 'package')
     package.set('name', 'gammu.exe')
     package.set('line-rate', str(line_rates['_']))
+    package.set('branch-rate', '0')
+    package.set('complexity', '0')
     classes = SubElement(package, 'classes')
 
     for item in data:
@@ -70,15 +87,14 @@ def write_data(data, handle):
         obj.set('name', item.rsplit('\\', 1)[1])
         obj.set('filename', item)
         obj.set('line-rate', str(line_rates[item]))
+        obj.set('branch-rate', '0')
+        obj.set('complexity', '0')
+        SubElement(obj, 'methods')
         lines = SubElement(obj, 'lines')
         for line in sorted(data[item], key=lambda x: int(x)):
             obj = SubElement(lines, 'line')
             obj.set('number', line)
             obj.set('hits', str(data[item][line]))
-
-    sources = SubElement(root, 'sources')
-    source = SubElement(sources, 'source')
-    source.text = 'c:'
 
     tree = ElementTree(root)
 
