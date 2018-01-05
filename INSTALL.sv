a60633dedd2813d8cb01b8bf23b243ce2d6126d1
Michal Čihař <michal@cihar.com>
2018-01-05 10:00:26 +0100

    Update po files and po4a docs
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ INSTALL.sv
@@ -0,0 +1,5 @@
+Gammu All Mobile Management Utilities - Installation
+====================================================
+
+Se docs/manual/project/install.rst för installationsguiden eller se kapitlet
+”Kompilera Gammu” i Gammu-manualen.
