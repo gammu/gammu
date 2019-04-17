9d19140ca1e4271c12edd853f0c25e1cd21a1770
Kenny Stuart <kstuart@hotmail.co.uk>
2019-04-17 22:37:35 +0100

    cleanup test to clearly check code only USSD is received without error.
    Replaced original test used during debugging session with one that tests
    the actual purpose of the patch.

--- tests/atgen/smsd-incoming-ussd.c
+++ tests/atgen/smsd-incoming-ussd.c
@@ -5,11 +5,9 @@
 #include "../../libgammu/gsmstate.h"
 #include "../../smsd/services/files.h"
 
-GSM_Error SMSD_ConfigureLogging(GSM_SMSDConfig *Config, gboolean uselog);
-GSM_Error SMSD_SendSMS(GSM_SMSDConfig *Config);
 void SMSD_IncomingUSSDCallback(GSM_StateMachine *sm, GSM_USSDMessage *ussd, void *user_data);
 
-int main(void)
+void receive_ussd_with_code_only(void)
 {
   GSM_Error error;
   GSM_StateMachine *s = setup_state_machine();
@@ -17,28 +15,9 @@ int main(void)
   GSM_SMSDStatus status;
   GSM_SMSDConfig *config = SMSD_NewConfig("test");
   GSM_Protocol_Message msg;
-  const char *event = "+CUSD: 2";// USSD terminate for issue #464
-
-  const char *responses[] = {
-    // AT+CSCS=?
-    "+CSCS: (\"IRA\",\"GSM\",\"UCS2\")\r",
-    "OK\r\n",
-    // AT+CUSD=1
-    "OK\r\n",
-    // AT+CSCS="UCS2"
-    "OK\r\n",
-    // AT+CSCS?
-    "+CSCS:\r",
-    "OK\r\n",
-    // USSD DialService
-    "+CUSD: 0,\"0076006F00740072006500200072006500710075006500740065002000650073007400200065006E00200063006F0075007200730020006400650020007400720061006900740065006D0065006E0074002E00200076006500750069006C006C0065007A0020006C0069007200650020006C00650020006D00650073007300610067006500200070006C0075007300200074006100720064002E\",15\r\n",
-    "OK\r\n",
-  };
+  const char *event = "+CUSD: 2";
 
   UNNEEDED(Priv);
-
-  SET_RESPONSES(responses);
-  bind_response_handling(s);
 
   error = SMSD_ReadConfig("smsd.cfg", config, TRUE);
   test_result(error == ERR_NONE);
@@ -47,12 +26,10 @@ int main(void)
   memset(&status, 0, sizeof(GSM_SMSDStatus));
   config->Status = &status;
 
-  GSM_SetIncomingUSSDCallback(s, &SMSD_IncomingUSSDCallback, config);
-  GSM_SetIncomingUSSD(s, TRUE);
-
-  error = GSM_DialService(s, (char*)"*772*51726568*125*1994#");
-  test_result(error == ERR_NONE);
-  test_result(status.Received == 1);
+  s->User.IncomingUSSD = &SMSD_IncomingUSSDCallback;
+  s->User.IncomingUSSDUserData = config;
+  s->Phone.Data.EnableIncomingUSSD = TRUE;
+  s->Phone.Data.RequestID = ID_None;
 
   msg.Length = strlen(event);
   msg.Buffer = (char*)event;
@@ -61,7 +38,12 @@ int main(void)
   s->Phone.Data.RequestMsg = &msg;
   error = ATGEN_DispatchMessage(s);
   test_result(error == ERR_NONE);
-  test_result(status.Received == 2);
+  test_result(status.Received == 1);
 
   SMSD_FreeConfig(config);
+}
+
+int main(void)
+{
+  receive_ussd_with_code_only();
 }
