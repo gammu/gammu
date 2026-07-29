9529ea5278e27a9f47054a9324d84fcc4d33db29
Boian Bonev <bbonev@ipacct.com>
2026-07-29 05:56:10 +0000

    Do not emit warnings in the documentation

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -122,7 +122,7 @@ pygments_style = "sphinx"
 # modindex_common_prefix = []
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
-# keep_warnings = False
+keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
